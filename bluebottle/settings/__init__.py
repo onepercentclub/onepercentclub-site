5c2349065bb8b33edce9450f5fca37b748123147
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-06-27 11:57:40 +0200

    More elegant handling of non-existing local exception.

--- bluebottle/settings/__init__.py
+++ bluebottle/settings/__init__.py
@@ -1,9 +1,11 @@
 # Smarter settings working, based on work by Rob Golding
 # http://www.robgolding.com/blog/2010/05/03/extending-settings-variables-with-local_settings-py-in-django/
 
+import sys
+
 try:
     from .local import *
 except ImportError:
-    print 'Local settings file not found. Please run `prepare.sh` to create one.'
-
-    exit(-1)
+    # Throw a return value of 1 and exit with a SystemExit exception
+    # Ref: http://docs.python.org/library/sys.html#sys.exit
+    sys.exit('Local settings file not found. Please run `prepare.sh` to create one.')
