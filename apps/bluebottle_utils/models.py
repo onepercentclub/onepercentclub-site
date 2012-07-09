700912cc3db69cfd99e33b715dcba7b6717aa225
Ben Konrath <ben@bagu.org>
2012-07-09 15:15:34 +0200

    Remove the address_ prefix from line1 and line2 in the Address model.
    The address_ prefix is redundant now that we have an Address model.

--- apps/bluebottle_utils/models.py
+++ apps/bluebottle_utils/models.py
@@ -6,9 +6,8 @@ class Address(models.Model):
     """
     A postal address.
     """
-
-    address_line1 = models.CharField(max_length=100, blank=True)
-    address_line2 = models.CharField(max_length=100, blank=True)
+    line1 = models.CharField(max_length=100, blank=True)
+    line2 = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=100, blank=True)
     state = models.CharField(max_length=100, blank=True)
     country = CountryField(blank=True)
