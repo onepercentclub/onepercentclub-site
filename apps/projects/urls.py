93cdd6c475bf78590870ea1c915db76df139f635
Loek van Gent <loek@1procentclub.nl>
2012-07-18 12:06:29 +0200

    Change urls structure for project details view

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('apps.projects.views',
-    url(r'^view/(?P<slug>.+)/$', 'view'),
+    url(r'^(?P<slug>.+)/$', 'view'),
 )
 
