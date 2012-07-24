95b91a79fb045f21268bf6f95b5efb63dee5e12d
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-07-24 15:54:44 +0200

    Factored out create_donation in DonationTestsMixin

--- apps/donations/tests.py
+++ apps/donations/tests.py
@@ -9,100 +9,113 @@ from apps.projects.tests import ProjectTestsMixin
 from .models import Donation, DonationLine
 
 
-class DonationTests(TestCase, ProjectTestsMixin, UserTestsMixin):
+class DonationTestsMixin(ProjectTestsMixin, UserTestsMixin):
+    """ Base class for tests using donations. """
+    def create_donation(self, user=None, amount=None):
+        if not user:
+            user = self.create_user()
+
+        if not amount:
+            amount = Decimal('10.00')
+
+        return Donation(user=user, amount=amount)
+
+    def create_donationline(self, donation=None, project=None, amount=None):
+        if not project:
+            project = self.create_project()
+            project.save()
+
+        if not donation:
+            donation = self.create_donation()
+            donation.save()
+
+        if not amount:
+            amount = Decimal('10.00')
+
+        return DonationLine(donation=donation, project=project, amount=amount)
+
+class DonationTests(TestCase, DonationTestsMixin):
     """ Tests for donations. """
-
-    def setUp(self):
-        """ Setup a user and a project. """
-
-        self.user = self.create_user()
-
-        self.project = self.create_project()
-        self.project.save()
 
     def test_donationsave(self):
         """ Test if saving a donation works. """
 
-        donation = Donation(user=self.user)
-        donation.amount = Decimal('20.00')
+        donation = self.create_donation()
         donation.save()
 
     def test_donationlinesave(self):
         """ Test if saving a donation line works. """
 
-        donation = Donation(user=self.user)
-        donation.amount = Decimal('20.00')
-        donation.save()
-
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('5.00')
+        donationline = self.create_donationline()
         donationline.save()
 
     def test_unicode(self):
         """ Test to see wheter unicode representations will fail or not. """
-        donation = Donation(user=self.user)
-        donation.amount = Decimal('21.00')
+        donation = self.create_donation()
+
         donation_str = unicode(donation)
         self.assertTrue(donation_str)
-        self.assertIn('21.00 on', donation_str)
+        self.assertIn('10.00 on', donation_str)
 
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('21.00')
+        donationline = self.create_donationline()
 
         donation_str = unicode(donationline)
         self.assertTrue(donation_str)
-        self.assertIn('21.00 from donation', donation_str)
+        self.assertIn('10.00 from donation', donation_str)
 
     def test_donationvalidation(self):
         """ Test validation for DonationLine objects. """
 
-        donation = Donation(user=self.user)
-        donation.amount = Decimal('20.00')
+        donation = self.create_donation(amount=Decimal('20.00'))
         donation.save()
 
         # 21 should raise an error because it's more than 20
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('21.00')
+        donationline = self.create_donationline(amount=Decimal('21.00'))
         self.assertRaises(ValidationError, donationline.full_clean)
 
         # 19 should be fine
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('19.00')
+        donationline = self.create_donationline(
+            amount=Decimal('19.00'), donation=donation
+        )
         donationline.full_clean()
 
         # 20 should be fine
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('20.00')
+        donationline = self.create_donationline(
+            amount=Decimal('20.00'), donation=donation
+        )
         donationline.full_clean()
 
         # Now check wheter it is actually updated
         # Add a donation of 5 and see whether 16 raises an error and
         # 15 does not
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('5.00')
+        donationline = self.create_donationline(
+            amount=Decimal('5.00'), donation=donation
+        )
         donationline.save()
 
         # 16 should now raise an error
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('16.00')
+        donationline = self.create_donationline(
+            amount=Decimal('16.00'), donation=donation
+        )
         self.assertRaises(ValidationError, donationline.full_clean)
 
         # 15 should be fine
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('15.00')
+        donationline = self.create_donationline(
+            amount=Decimal('15.00'), donation=donation
+        )
         donationline.full_clean()
 
     def test_donationupdate(self):
         """ Regression test for updating donationlines. """
 
-        donation = Donation(user=self.user)
-        donation.amount = Decimal('20.00')
+        donation = self.create_donation(amount=Decimal('20.00'))
         donation.save()
 
         # 21 should raise an error because it's more than 20
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('20.00')
-        donationline.save()
+        donationline = self.create_donationline(
+            amount=Decimal('21.00'), donation=donation
+        )
+        self.assertRaises(ValidationError, donationline.full_clean)
 
         # Updating to 19 should be fine
         donationline.amount = Decimal('19.00')
@@ -117,30 +130,25 @@ class DonationTests(TestCase, ProjectTestsMixin, UserTestsMixin):
         base classes in `base.py` instead of having everything in one file.
         """
 
-        donation = Donation(user=self.user)
-        donation.amount = Decimal('20.00')
-        donation.save()
-
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('5.00')
+        donationline = self.create_donationline()
         donationline.save()
 
         # Test wether a single supporter is properly listed
-        supporters = list(self.project.get_supporters())
+        supporters = list(donationline.project.get_supporters())
 
-        self.assertEqual(supporters, [self.user])
+        self.assertEqual(supporters, [donationline.donation.user])
 
         # Add another
         other_user = self.create_user()
 
-        donation = Donation(user=other_user)
-        donation.amount = Decimal('20.00')
+        donation = self.create_donation(user=other_user)
         donation.save()
 
-        donationline = DonationLine(donation=donation, project=self.project)
-        donationline.amount = Decimal('5.00')
+        donationline = self.create_donationline(
+            donation=donation, project=donationline.project
+        )
         donationline.save()
 
-        supporters = self.project.get_supporters()
+        supporters = donationline.project.get_supporters()
         self.assertIn(other_user, supporters)
         self.assertEquals(supporters.count(), 2)
