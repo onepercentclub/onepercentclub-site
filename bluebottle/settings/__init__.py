1ed9df901d82b1e02699f68db4e707475162b826
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-08-15 17:15:38 +0200

    Revert __init__.py to old state.

--- bluebottle/settings/__init__.py
+++ bluebottle/settings/__init__.py
@@ -1,7 +1,11 @@
-# Default to local settings override, if available.
-# If not, default to development settings.
+# Smarter settings working, based on work by Rob Golding
+# http://www.robgolding.com/blog/2010/05/03/extending-settings-variables-with-local_settings-py-in-django/
+
+import sys
 
 try:
     from .local import *
 except ImportError:
-    from .development import *
+    # Throw a return value of 1 and exit with a SystemExit exception
+    # Ref: http://docs.python.org/library/sys.html#sys.exit
+    sys.exit('Local settings file not found. Please run `prepare.sh` to create one.')
