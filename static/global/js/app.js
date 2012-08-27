b832c3ac10d345396d3692b432ba2f0f64a67e9f
Loek van Gent <loek@1procentclub.nl>
2012-08-27 14:36:25 +0200

    Add basic projects page to Ember Application.
    Routing and main navigation

--- static/global/js/app.js
+++ static/global/js/app.js
@@ -1,39 +1,78 @@
 (function() {
 
-// Create the application
-window.App = Ember.Application.create({
-  rootElement: '#content',
+  // Create the application
+  window.App = Ember.Application.create({
+    rootElement : '#content',
 
-  // Define the main application controller. This is automatically picked up by
-  // the application and initialized.
-  ApplicationController: Ember.Controller.extend({
-  }),
-  
-  ApplicationView: Ember.View.extend({
-    templateName: 'application'
-  }),
+    // Define the main application controller. This is automatically picked up by
+    // the application and initialized.
+    ApplicationController : Ember.Controller.extend({
+    }),
 
+    ApplicationView : Ember.View.extend({
+      templateName : 'application'
+    }),
 
-  HomeView: Ember.View.extend({
-    templateName: 'home'
-  }),
+    // Use this to clear an outlet
+    EmptyView : Ember.View.extend({
+      template : ''
+    }),
 
+    /* Home */
+    HomeView : Ember.View.extend({
+      templateName : 'home'
+    }),
 
-  Router: Ember.Router.extend({
-    root: Ember.Route.extend({
-      home: Ember.Route.extend({
-        route: '/',
-        connectOutlets: function(router, event) {
-          router.get('applicationController').connectOutlet('topPanel', 'home');
-        }
-      }),
+    /* Projects */
+    ProjectsController : Ember.Controller.extend({
+    }),
+
+    ProjectsHomeView : Ember.View.extend({
+      templateName : 'projects-home'
+    }),
+
+    ProjectsSearchFormView : Ember.View.extend({
+      templateName : 'projects-search-form'
+    }),
+
+    ProjectsSearchResultsView : Ember.View.extend({
+      templateName : 'projects-search-results'
+    }),
+
+    /* Routing */
+    Router : Ember.Router.extend({
+      root : Ember.Route.extend({
+        // Used for navigation
+        gotoHome : function(router, event) {
+          router.transitionTo('home');
+        },
+        gotoProjects : function(router, event) {
+          router.transitionTo('projects');
+        },
+        // The actual routing
+        home : Ember.Route.extend({
+          route : '/',
+          connectOutlets : function(router, event) {
+            router.get('applicationController').connectOutlet('topPanel', 'home');
+            router.get('applicationController').connectOutlet('midPanel', 'empty');
+            router.get('applicationController').connectOutlet('bottomPanel', 'empty');
+          }
+        }),
+        projects : Ember.Route.extend({
+          route : '/projects',
+          connectOutlets : function(router, event) {
+            router.get('applicationController').connectOutlet('topPanel', 'projectsHome');
+            router.get('applicationController').connectOutlet('midPanel', 'projectsSearchForm');
+            router.get('applicationController').connectOutlet('bottomPanel', 'projectsSearchResults');
+          }
+        }),
+      })
     })
-  })
 
-});
+  });
 
-$(function() {
-  App.initialize();
-});
+  $(function() {
+    App.initialize();
+  });
 
-})();
+})(); 
