df5ffb9434d8c607ed256ff94c5329e238bcc57a
Ben Konrath <ben@1procentclub.nl>
2012-11-07 20:21:52 +0100

    Add a few more tests for reactions api.

--- apps/reactions/tests.py
+++ apps/reactions/tests.py
@@ -43,29 +43,85 @@ class ReactionApiIntegrationTest(BlogPostCreationMixin, TestCase):
 
     def setUp(self):
         # TODO: Add a test for reaction on another type of content.
-        self.blogpost1 = self.create_blogpost()
-
+        self.blogpost = self.create_blogpost()
         self.list_view = BlogPostList.as_view()
         self.detail_view = BlogPostDetail.as_view()
         self.api_base = '/i18n/api/blogs/'
         self.reaction_api_name = '/reactions/'
+        self.reactions_url = self.api_base + self.blogpost.slug + self.reaction_api_name
 
         self.client = Client(enforce_csrf_checks=False)
-        self.client.login(username=self.blogpost1.author.username, password='password')
+        self.client.login(username=self.blogpost.author.username, password='password')
 
     def tearDown(self):
         self.client.logout()
 
-    def test_create_and_retrieve_reaction(self):
+    def test_reaction_crud(self):
         """
-        Basic test for creating and retrieving a reaction.
+        Tests for creating, retrieving, updating and deleting a reaction.
         """
+
+        # Create reaction.
         reaction_text = 'Great job!'
-        response = self.client.post(self.api_base + self.blogpost1.slug + self.reaction_api_name, {'reaction': reaction_text})
+        response = self.client.post(self.reactions_url, {'reaction': reaction_text})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['reaction'], reaction_text)
 
-        id = response.data['id']
-        response = self.client.get(self.api_base + self.blogpost1.slug + self.reaction_api_name + str(id))
+        # Retrieve reaction.
+        reaction_detail_url = self.reactions_url + str(response.data['id'])
+        response = self.client.get(reaction_detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['reaction'], reaction_text)
+
+        # Update reaction.
+        new_reaction_text = 'This is a really nice post.'
+        response = self.client.put(reaction_detail_url, {'reaction': new_reaction_text})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['reaction'], new_reaction_text)
+
+        # Delete reaction.
+        response = self.client.delete(reaction_detail_url, {'reaction': new_reaction_text})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+    def test_reactions_on_multiple_objects(self):
+        """
+        Tests for creating, retrieving, updating and deleting a reaction.
+        """
+
+        # Create two reactions.
+        reaction_text_1 = 'Great job!'
+        response = self.client.post(self.reactions_url, {'reaction': reaction_text_1})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['reaction'], reaction_text_1)
+
+        reaction_text_2 = 'This is a really nice post.'
+        response = self.client.post(self.reactions_url, {'reaction': reaction_text_2})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['reaction'], reaction_text_2)
+
+        # Check the size of the reaction list is correct.
+        response = self.client.get(self.reactions_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 2)
+
+        # Create a reaction on second blog post.
+        second_blogpost = self.create_blogpost(title='Ben Jij Rijk?')
+        second_reactions_url = self.api_base + second_blogpost.slug + self.reaction_api_name
+        reaction_text_3 = 'Super!'
+        response = self.client.post(second_reactions_url, {'reaction': reaction_text_3})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['reaction'], reaction_text_3)
+
+        # Check that the size and data in the first reaction list is correct.
+        response = self.client.get(self.reactions_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 2)
+        self.assertEqual(response.data['results'][0]['reaction'], reaction_text_1)
+        self.assertEqual(response.data['results'][1]['reaction'], reaction_text_2)
+
+        # Check that the size and data in the second reaction list is correct.
+        response = self.client.get(second_reactions_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+        self.assertEqual(response.data['results'][0]['reaction'], reaction_text_3)
