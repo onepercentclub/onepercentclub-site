8139a3a49e4ee6d1fc8a2a71becbfd6d625b5c5f
Ben Konrath <ben@1procentclub.nl>
2012-11-08 16:19:41 +0100

    Add picture field member detail api.

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,3 +1,4 @@
+from apps.bluebottle_utils.serializers import SorlImageField
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.fields import HyperlinkedIdentityField
@@ -6,10 +7,11 @@ from rest_framework.fields import HyperlinkedIdentityField
 class MemberDetailSerializer(serializers.ModelSerializer):
 
     url = HyperlinkedIdentityField(view_name='member-detail')
+    picture = SorlImageField('userprofile.picture', '100x100', crop='center')
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'username', 'url')
+        fields = ('id', 'first_name', 'last_name', 'username', 'url', 'picture')
 
 
 class MemberListSerializer(MemberDetailSerializer):
