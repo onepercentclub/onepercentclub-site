b4c88bcedaf6cab078426675ddbaf0e963b4a821
Ben Konrath <ben@1procentclub.nl>
2012-11-09 14:23:45 +0100

    Make reaction api display user id instead of username.

--- apps/reactions/serializers.py
+++ apps/reactions/serializers.py
@@ -1,8 +1,7 @@
-from apps.bluebottle_utils.serializers import SorlImageField, SlugHyperlinkedIdentityField
+from apps.bluebottle_utils.serializers import SorlImageField
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Reaction
-from rest_framework.fields import HyperlinkedIdentityField
 
 
 class ReactionAuthorSerializer(serializers.ModelSerializer):
@@ -20,7 +19,7 @@ class ReactionDetailSerializer(serializers.ModelSerializer):
     # Custom fields.
 #   TODO: Enable embedded models in Ember Data and re-enable this.
 #    author = ReactionAuthorSerializer()
-    author = serializers.Field()  # Needed to make the author field read-only.
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
 #    TODO: This isn't working with the pattern: api/blogs/<slug>/reactions/<pk>
 #          Delete or fix this ... we don't really need it so removing it is ok but it's nice to have.
 #    url = HyperlinkedIdentityField(view_name='reactions:reaction-detail')
