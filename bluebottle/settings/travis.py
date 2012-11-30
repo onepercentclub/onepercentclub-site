97ffd9f5271ffb93b04da06866591f6e6650d76b
Ben Konrath <ben@1procentclub.nl>
2012-11-30 23:14:52 +0100

    Fix Travis config so that the test run.

--- bluebottle/settings/travis.py
+++ bluebottle/settings/travis.py
@@ -1,3 +1,9 @@
+# NOTE: local.py must be an empty file when using this configuration.
+
+from .defaults import *
+
+# Put the travis-ci environment specific overrides below.
+
 SECRET_KEY = 'hbqnTEq+m7Tk61bvRV/TLANr3i0WZ6hgBXDh3aYpSU8m+E1iCtlU3Q=='
 
 DATABASES = {
