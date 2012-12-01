a4475eb909b5124013796535fe89849877e4867c
Ben Konrath <ben@1procentclub.nl>
2012-12-01 13:39:53 +0100

    Fix formatting for projects.js.

--- static/global/js/app/projects.js
+++ static/global/js/app/projects.js
@@ -1,10 +1,13 @@
 App.Country = DS.Model.extend({
     name: DS.attr('string'),
-    subregion: DS.attr('string'),
+    subregion: DS.attr('string')
 })
+
 
 App.Project = DS.Model.extend({
     url: 'projects',
+
+    // Model fields
     slug: DS.attr('string'),
     title: DS.attr('string'),
     image: DS.attr('string'),
@@ -16,14 +19,14 @@ App.Project = DS.Model.extend({
     tags: DS.attr('array'),
     owner: DS.belongsTo('App.Member', {embedded: true}),
     country: DS.belongsTo('App.Country', {embedded: true}),
-   // For now we set some default values here because we don't have actual numbers
+
+    // FIXME: For now we set some default values here because we don't have actual numbers
     supporter_count: DS.attr('number', {defaultValue: 123}),
     days_left: DS.attr('number', {defaultValue: 123}),
 
-
-    // For now we do some html generating here.
+    // FIXME: For now we do some html generating here.
     // TODO: solve this in Ember or Handlebars helper
-    days_left_span: function(){
+    days_left_span: function() {
         var dl = this.get('days_left').toString();
         var html = '';
         for (var i = 0, len = dl.length; i < len; i += 1) {
@@ -32,7 +35,7 @@ App.Project = DS.Model.extend({
         return html;
     }.property('days_left'),
 
-    supporter_count_span: function(){
+    supporter_count_span: function() {
         var dl = this.get('supporter_count').toString();
         var html = '';
         for (var i = 0, len = dl.length; i < len; i += 1) {
@@ -40,33 +43,31 @@ App.Project = DS.Model.extend({
         }
         return html;
     }.property('supporter_count')
-    
-    
 })
 
+
 App.projectSearchController = App.ListController.create({
-    model: App.Project,
+    model: App.Project
 });
 
+
 /* The search results. */
-App.ProjectSearchView  = Em.View.extend({
+App.ProjectSearchView = Em.View.extend({
     countBinding: 'App.projectSearchController.count',
     tagName: 'div',
     templateName: 'project_search',
-    classNames: ['container'],
-
+    classNames: ['container']
 });
+
 
 App.ProjectSearchResultsView = Em.CollectionView.extend({
     contentBinding: "App.projectSearchController",
     tagName: 'ul',
     emptyView: Em.View.extend({
-      templateName: 'project_no_results',
-      templateFile: 'project_search'
+        templateName: 'project_no_results',
+        templateFile: 'project_search'
     }),
-    itemViewClass: 'App.ProjectPreviewView',
-
-
+    itemViewClass: 'App.ProjectPreviewView'
 });
 
 
@@ -75,6 +76,7 @@ App.ProjectPreviewView = Em.View.extend({
     templateName: 'project_preview',
     templateFile: 'project_search'
 });
+
 
 App.ProjectSupportersView = Em.View.extend({
     contentBinding: "App.projectDetailController",
@@ -92,7 +94,7 @@ App.projectDetailController = App.DetailController.create({
 App.ProjectDetailView = Em.View.extend({
     contentBinding: 'App.projectDetailController',
     templateName: 'project_detail',
-    classNames: ['lightgreen', 'section'],
+    classNames: ['lightgreen', 'section']
 });
 
 
@@ -102,9 +104,11 @@ App.ProjectTagsView = Em.CollectionView.extend({
     tagName: 'ul',
     classNames: ['project-tags'],
     emptyView: Em.View.extend({
-      template: function(){ return '<li class="none">none</li>';}
+        template: function() {
+            return '<li class="none">none</li>';
+        }
     }),
-    itemViewClass: 'App.ProjectTagItemView',
+    itemViewClass: 'App.ProjectTagItemView'
 });
 
 
@@ -112,36 +116,35 @@ App.ProjectTagItemView = Em.View.extend({
     tagName: 'li',
     limitBinding: 'parentView.limit',
     classNameBindings: ['outOfRange'],
-    outOfRange: function(){
-        if(this.get('contentIndex') > this.get('limit')){
-             return 'out-of-range';
+    outOfRange: function() {
+        if (this.get('contentIndex') > this.get('limit')) {
+            return 'out-of-range';
         }
     }.property('contentIndex'),
     templateName: 'project_tag_item',
     templateFile: 'project_detail'
 });
 
+
 App.ProjectTagsMoreView = Em.View.extend({
     limit: 4,
     contentBinding: "App.projectDetailController.content.tags",
-    more: function(){
+    more: function() {
         if (this.get('content') == undefined) {
             return 0;
         }
         return this.get('content').length - this.get('limit');
     }.property('limit', 'content'),
     classNameBindings: ['outOfRange'],
-    outOfRange: function(){
+    outOfRange: function() {
         if (this.get('content') == undefined) {
             return 'hidden';
         }
-        if(this.get('content').length <= this.get('limit')){
-             return 'hidden';
+        if (this.get('content').length <= this.get('limit')) {
+            return 'hidden';
         }
     }.property('content'),
     tagName: 'span',
     templateName: 'project_tags_more',
     templateFile: 'project_detail'
 });
-
-
