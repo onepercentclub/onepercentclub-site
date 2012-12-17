1096eeb344d7ff98ede2baea69705b5cc6b4b7fe
Ben Konrath <ben@1procentclub.nl>
2012-12-17 13:23:15 +0100

    Allow text wall posts to have a null author.

--- apps/wallposts/models.py
+++ apps/wallposts/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django_extensions.db.fields import ModificationDateTimeField, CreationDateTimeField
+from django_extensions.utils.text import truncate_letters
 from polymorphic import PolymorphicModel
 
 
 class WallPost(PolymorphicModel):
-    # The user who wrote the wall post.
-    author = models.ForeignKey('auth.User')
-
     # The metadata for the wall post.
     created = CreationDateTimeField()
     updated = ModificationDateTimeField()
@@ -21,25 +19,27 @@ class WallPost(PolymorphicModel):
 
 
 class MediaWallPost(WallPost):
+    # The user who wrote the wall post.
+    author = models.ForeignKey('auth.User')
+
+    # The content of the wall post
     title = models.CharField(max_length=60)
     text = models.TextField(max_length=300, blank=True)
     video_url = models.URLField(max_length=100, blank=True)
 
     def __unicode__(self):
         text = ""
-        if self.text and len(self.text) > 50:
-            text = ": " + self.text[:50]
-        elif self.text:
-            text = ": " + self.text
+        if self.text:
+            text = ": " + truncate_letters(self.text, 50)
         return self.__class__.__name__ + text
 
 
 class TextWallPost(WallPost):
+    # The user who wrote the wall post - can be empty to support anonymous text wallposts.
+    author = models.ForeignKey('auth.User', blank=True, null=True)
+
+    # The content of the wall post
     text = models.TextField(max_length=300)
 
     def __unicode__(self):
-        if len(self.text) > 50:
-            text = self.text[:50]
-        else:
-            text = self.text
-        return self.__class__.__name__ + ": " + text
+        return self.__class__.__name__ + ": " + truncate_letters(self.text, 50)
