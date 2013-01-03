cc3ade7c75ecb37874ab7e81453be9d2def3892f
Loek van Gent <loek@1procentclub.nl>
2013-01-03 12:28:23 +0100

    Clean-ups and documentation

--- apps/projects/permissions.py
+++ apps/projects/permissions.py
@@ -11,24 +11,22 @@ class IsProjectOwner(permissions.IsAuthenticated):
         # If called from another API through JSON project_id should be in request.DATA
         if request.DATA:
             project_id = request.DATA.get('project_id', None)
-        else:
-            # This is just here for use of the HTML API
+        elif request.QUERY_PARAMS:
+            # This is just here for use of the API html interface
             project_id = request.QUERY_PARAMS.get('project_id', None)
-        print project_id
         if project_id:
             project = Project.objects.get(pk=project_id)
         if not project_id or not project:
-            # TODO: We should throw some nice Exception here!
+            # TODO: Should throw some Exception here?
             return None
         return project
     
 
     def has_permission(self, request, view, obj=None):
         # If called from projects API we can expect obj to be a Project
-        if isinstance(obj, Project):
-            project = obj
-        else:
-            project = self.get_project_from_request(request)
+        # or it might be available through view.get_object()
+        # for now we just get it from the request.
+        project = self.get_project_from_request(request)
         if project and request.user and request.user.is_authenticated() and project.owner == request.user:
             return True
         return False
@@ -39,10 +37,10 @@ class IsProjectOwnerOrReadOnly(IsProjectOwner):
     Allows access only to project owner.
     """
     def has_permission(self, request, view, obj=None):
-        if isinstance(obj, Project):
-            project = obj
-        else:
-            project = self.get_project_from_request(request)
+        # If called from projects API we can expect obj to be a Project
+        # or it might be available through view.get_object()
+        # for now we just get it from the request.
+        project = self.get_project_from_request(request)
         if (request.method in permissions.SAFE_METHODS or 
             project and request.user and request.user.is_authenticated() 
             and project.owner == request.user):
--- apps/wallposts/permissions.py
+++ apps/wallposts/permissions.py
@@ -6,7 +6,9 @@ class IsAuthor(permissions.IsAuthenticated):
     Allows write access to author.
     """
     def has_permission(self, request, view, obj=None):
-        if obj and request.user and request.user.is_authenticated() and obj.author == request.user:
+        if not obj:
+            obj = view.get_object()
+        if request.user and request.user.is_authenticated() and obj.author == request.user:
             return True
         return False
 
@@ -15,9 +17,10 @@ class IsAuthorOrReadOnly(IsAuthor):
     Allows write access to author otherwise read access.
     """
     def has_permission(self, request, view, obj=None):
-        print obj
-        print request.DATA
-        print request.QUERY_PARAMS
+        # If we don't have an object, we get it from the view.
+        # Don't know why the view isn't passing it in the first place...
+        if not obj:
+            obj = view.get_object()
         if (request.method in permissions.SAFE_METHODS or 
             request.user and request.user.is_authenticated() and obj.author == request.user):
             return True
