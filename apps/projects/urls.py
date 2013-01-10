9b49601e0cbb9a3d46d50bdfbad2b43152048f23
Loek van Gent <loek@1procentclub.nl>
2013-01-10 15:42:31 +0100

    Move Project WallPosts to projects app

--- apps/projects/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns
-
-from surlex.dj import surl
-
-from .views import ProjectDetailView
-
-urlpatterns = patterns('',
-    surl(r'^<slug:s>$', ProjectDetailView.as_view(), name='project-detail'),
-)
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,12 +1,15 @@
-from django.views.generic.detail import DetailView
-from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
+from django.contrib.contenttypes.models import ContentType
+from apps.bluebottle_drf2.views import ListCreateAPIView, RetrieveUpdateDeleteAPIView, ListAPIView
+from apps.bluebottle_utils.utils import get_client_ip
+from apps.projects.permissions import IsProjectOwnerOrReadOnly
+from apps.bluebottle_drf2.permissions import IsAuthorOrReadOnly
+from apps.wallposts.serializers import ProjectWallPostSerializer, ProjectMediaWallPostSerializer, ProjectTextWallPostSerializer
+from apps.wallposts.models import WallPost, MediaWallPost, TextWallPost
 from .models import Project
 from .serializers import ProjectSerializer
 
-
-# API views
 
 class ProjectList(generics.ListAPIView):
     model = Project
@@ -22,9 +25,60 @@ class ProjectDetail(generics.RetrieveAPIView):
     permissions_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
-# Django template Views
+class ProjectWallPostMixin(object):
 
-class ProjectDetailView(DetailView):
-    model = Project
-    template_name = 'project_detail.html'
-    
+    def get_queryset(self):
+        queryset = super(ProjectWallPostMixin, self).get_queryset()
+        project_type = ContentType.objects.get_for_model(Project)
+        queryset = queryset.filter(content_type=project_type)
+        project_id = self.request.QUERY_PARAMS.get('project_id', None)
+        if project_id:
+            queryset = queryset.filter(object_id=project_id)
+        queryset = queryset.order_by("-created")
+        return queryset
+
+    def pre_save(self, obj):
+        if not obj.author:
+            obj.author = self.request.user
+        else:
+            obj.editor = self.request.user
+        obj.ip_address = get_client_ip(self.request)
+
+
+class ProjectWallPostList(ProjectWallPostMixin, ListAPIView):
+    model = WallPost
+    serializer_class = ProjectWallPostSerializer
+    paginate_by = 10
+
+
+class ProjectWallPostDetail(ProjectWallPostMixin, RetrieveUpdateDeleteAPIView):
+    model = WallPost
+    serializer_class = ProjectWallPostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+
+class ProjectMediaWallPostList(ProjectWallPostMixin, ListCreateAPIView):
+    model = MediaWallPost
+    serializer_class = ProjectMediaWallPostSerializer
+    permission_classes = (IsProjectOwnerOrReadOnly,)
+    paginate_by = 10
+
+
+class ProjectMediaWallPostDetail(ProjectWallPostMixin, RetrieveUpdateDeleteAPIView):
+    model = MediaWallPost
+    serializer_class = ProjectMediaWallPostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+
+class ProjectTextWallPostList(ProjectWallPostMixin, ListCreateAPIView):
+    model = TextWallPost
+    serializer_class = ProjectTextWallPostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    paginate_by = 10
+
+
+class ProjectTextWallPostDetail(ProjectWallPostMixin, RetrieveUpdateDeleteAPIView):
+    model = TextWallPost
+    serializer_class = ProjectTextWallPostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
