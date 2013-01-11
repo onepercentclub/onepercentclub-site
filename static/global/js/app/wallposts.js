9c3cfeec5ac86229d1c87bb88ca209362210d5d5
Ben Konrath <ben@1procentclub.nl>
2013-01-11 10:27:51 +0100

    Properly add wallpost to top of list.

--- static/global/js/app/wallposts.js
+++ static/global/js/app/wallposts.js
@@ -16,14 +16,7 @@ App.ProjectWallPost = DS.Model.extend({
     created: DS.attr('string'),
     timesince: DS.attr('string'),
     video_url: DS.attr('string'),
-    video_html: DS.attr('string'),
-
-    // TODO: Find a better way to refresh the list
-    didCreate: function() {
-        App.store.find(App.ProjectWallPost, this.get('id'));
-        App.projectWallPostListController.set('content', App.store.all(App.ProjectWallPost));
-    }
-
+    video_html: DS.attr('string')
 });
 
 
@@ -49,12 +42,22 @@ App.projectWallPostListController = Em.ArrayController.create({
     },
     
     projectBinding: "App.projectDetailController.content",
-    
-    content: function(){
-        if (this.get('project.id')) {
-            return App.ProjectWallPost.find({project_id: this.get('project.id')})
+
+    projectChanged: function(sender, key) {
+        // The RecordArray is not loaded into 'content' so that we can use a real ember array
+        // for 'content' when it's loaded.
+        var projectId = this.get('project.id');
+        this.set('wallposts', App.ProjectWallPost.find({project_id: projectId}));
+    }.observes('project'),
+
+    wallpostsLoaded: function(sender, key) {
+        // Set 'content' with an ember array when the wallposts have been loaded. This allows
+        // us to manually manipulate 'content' when new posts are added.
+        if (sender.get(key)) {
+            sender.set('content', sender.get('wallposts').toArray());
         }
-    }.property('project'),
+    }.observes('wallposts.isLoaded'),
+
 
     addWallPost: function(wallpost) {
         // Load the right model based on type
@@ -63,8 +66,12 @@ App.projectWallPostListController = Em.ArrayController.create({
         if (!(wallpost instanceof model)) {
             // Add project_id to the wallpost
             wallpost.project_id = this.get('project.id');
-            var wallpost = model.createRecord(wallpost);
-        } 
+            var wallpost = App.ProjectMediaWallPost.createRecord(wallpost);
+        }
+        // Not a race condition because ember-data only starts its machinery when App.store.commit() is called.
+        wallpost.on('didCreate', function(record) {
+            App.projectWallPostListController.get('content').unshiftObject(record);
+        });
         App.store.commit();
         // Return the model (with errors) for the form to return it to the user.
         return wallpost;
@@ -98,17 +105,16 @@ App.MediaWallPostFormView = Em.View.extend({
     tagName: 'form',
     wallpost: {type: 'media'},
     submit: function(e){
+        // **** Why is this called when the project page is first loaded? ******
         e.preventDefault();
         // Send the object to controller
         // This will return a DS.Model with optional error codes
         var wallpost = App.projectWallPostListController.addWallPost(this.get('wallpost'));
-        // For now always return true so we get the errors
-        // TODO: Find out how we can properly check for errors
-        if (1 || wallpost.errors) {
+        if (wallpost.get('isError')) {
             // We'll replace this.wallpost with the proper DS.Model to bind errors
             this.set('wallpost', wallpost);
         } else {
-            // Wallpost was created succesfully so clear the form:
+            // Wallpost was created successfully so clear the form:
             this.set('wallpost', {type: wallpost.type});
         }
     }
@@ -117,7 +123,7 @@ App.MediaWallPostFormView = Em.View.extend({
 
 App.TextWallPostFormView = App.MediaWallPostFormView.extend({
     templateName: 'text_wallpost_form',
-    wallpost: {type: 'text'},
+    wallpost: {type: 'text'}
 });
 
 
@@ -149,9 +155,9 @@ App.WallPostView = Em.View.extend({
             // TODO: Have a proper solution for belongsTo fields in adapter
             post.reopen({
                 author: null
-            })
+            });
             post.deleteRecord();
-            App.store.commit()
+            App.store.commit();
             var self = this;
             this.$().slideUp(1000, function(){self.remove();});
         }
