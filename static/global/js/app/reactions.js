595b30d05bf28ed56edf0237cb1b6ccb009f2d0a
Loek van Gent <loek@1procentclub.nl>
2013-01-21 17:00:43 +0100

    some spacing and minor adjustments

--- static/global/js/app/reactions.js
+++ static/global/js/app/reactions.js
@@ -1,5 +1,6 @@
 App.Reaction = DS.Model.extend({
     url: 'reactions',
+
     reaction: DS.attr('string'),
     author: DS.belongsTo('App.Member', {embedded: true}),
     created: DS.attr('string'),
@@ -9,16 +10,18 @@ App.Reaction = DS.Model.extend({
 
 App.WallPostReaction = App.Reaction.extend({
     url: 'wallposts/reactions',
+
     // We need wallpost_id to create reactions in the API
     // This can't be a calculated property because then it won't be part of the API call
     wallpost_id: DS.attr('number'),
-    wallpost: DS.belongsTo('App.ProjectWallPost'),
+    wallpost: DS.belongsTo('App.ProjectWallPost')
 });
 
 
 App.wallPostReactionController = Em.Controller.create({
     model: App.WallPostReaction,
-    addReaction: function(reaction, wallpost){
+
+    addReaction: function(reaction, wallpost) {
         // Do a client side check if Reaction as a reaction property set
         // wallpost.reactions has problems with invalid records an will barf
         if (reaction.get('reaction') == undefined || reaction.get('reaction') == "") {
@@ -31,9 +34,9 @@ App.wallPostReactionController = Em.Controller.create({
         // Set the wallpost so the list gets updated in the view
         livereaction.set('wallpost_id', wallpost.get('id'));
         livereaction.set('wallpost', wallpost);
-        livereaction.on('didCreate', function(record){
+        livereaction.on('didCreate', function(record) {
             // Clear the reaction text in the form
-            reaction.set('errors', '');
+            reaction.set('errors', null);
             reaction.set('reaction', '');
         });
         transaction.commit();
@@ -46,15 +49,17 @@ App.WallPostReactionFormView = Em.View.extend({
     templateFile: 'reactions',
     tagName: 'form',
     classNames: ['reaction-form'],
+
     wallpostBinding: "parentView.content",
-    content: function(){
-        return App.wallPostReactionController.model.createRecord();
-    }.property('model'),
-    submit: function(e){
+
+    content: App.wallPostReactionController.get('model').createRecord(),
+
+    submit: function(e) {
         e.preventDefault();
         App.wallPostReactionController.addReaction(this.get('content'), this.get('wallpost'));
     },
-    didInsertElement: function(e){
+
+    didInsertElement: function(e) {
         this.$('textarea').focus(function(e) {
             $(this).parents('.reaction-form').addClass('is-selected');
         });
@@ -70,6 +75,7 @@ App.ReactionView = Em.View.extend({
     templateFile: 'reactions',
     tagName: 'li',
     classNames: ['initiator'],
+
     isAuthor: function(){
         var username = this.get('user.username');
         var authorname = this.get('content.author.username');
@@ -78,6 +84,7 @@ App.ReactionView = Em.View.extend({
         }
         return false;
     }.property('user', 'content'),
+
     deleteReaction: function(e) {
         if (confirm("Delete this reaction?")) {
             e.preventDefault();
