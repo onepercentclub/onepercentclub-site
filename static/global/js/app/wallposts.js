ac4b7652626fe82a7a2bfd5a42daf7108d5773a4
Ben Konrath <ben@1procentclub.nl>
2013-01-26 12:01:05 +0100

    Fix comment.

--- static/global/js/app/wallposts.js
+++ static/global/js/app/wallposts.js
@@ -66,7 +66,7 @@ App.WallPostControllerMixin = Em.Mixin.create({
 
 App.ProjectWallPostListController = Em.ArrayController.extend({
 
-    // The list of WallProjectWallPost are loaded into that temporary wallposts array in the Route. Once this RecordArray
+    // The list of WallPosts are loaded into the temporary 'wallposts' array in the Route. Once this RecordArray
     // is loaded, it is converted to an Ember array and put into content. This temporary array is required
     // because the RecordArray returned by findQuery can't be manipulated directly. Discussion about this
     // can be found in these two pages:
