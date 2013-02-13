4fbff08bad3f249a7ffef86ffe0becfcd9c31a4b
Ben Konrath <ben@1procentclub.nl>
2013-02-13 18:48:16 +0100

    Implement payment method restrictions for docdata.

--- apps/cowry_docdata/tests.py
+++ apps/cowry_docdata/tests.py
@@ -17,25 +17,37 @@ except (ConnectionError, AttributeError):
 
 class DocDataPaymentTests(TestCase):
 
-    def setUp(self):
-        if run_docdata_tests:
-            self.payment = factory.create_payment_object('MASTERCARD', amount=2000, currency='EUR')
-            self.payment.country = 'NL'
-            self.payment.city = 'Amsterdam'
-            self.payment.street = 'Dam'
-            self.payment.postal_code = '1001AM'
-            self.payment.first_name = 'Nijntje'
-            self.payment.last_name = 'het Konijnje'
-            self.payment.email = 'nijntje@hetkonijnje.nl'
-            self.payment.save()
-            payments.create_remote_payment_order(self.payment)
-
     @unittest.skipUnless(run_docdata_tests, 'DocData credentials not set or not online')
-    def test_setup_payment(self):
+    def test_basic_payment(self):
+        # Create the payment.
+        self.payment = factory.create_payment_object('MASTERCARD', amount=2000, currency='EUR')
+        self.payment.country = 'NL'
+        self.payment.city = 'Amsterdam'
+        self.payment.street = 'Dam'
+        self.payment.postal_code = '1001AM'
+        self.payment.first_name = 'Nijntje'
+        self.payment.last_name = 'het Konijnje'
+        self.payment.email = 'nijntje@hetkonijnje.nl'
+        self.payment.save()
+        payments.create_remote_payment_order(self.payment)
+
+        # Check that the order key has been saved.
         self.assertTrue(self.payment.payment_order_key)
 
-    @unittest.skipUnless(run_docdata_tests, 'DocData credentials not set or not online')
-    def test_setup_payment_url(self):
+        # Test that the payment url works.
         payment_url = payments.get_payment_url(self.payment)
         response = requests.get(payment_url)
         self.assertEqual(response.status_code, 200)
+
+    @unittest.skipUnless(run_docdata_tests, 'DocData credentials not set or not online')
+    def test_payment_method_restrictions(self):
+        # Test country restrictions.
+        payment_methods = factory.get_payment_methods(country='NL')
+        self.assertTrue('IDEAL' in payment_methods, "IDEAL not is in payment method list.")
+        self.assertTrue(len(payment_methods) > 1, "Payment method list should have two or more payment methods.")
+        payment_methods = factory.get_payment_methods(country='CA')
+        self.assertTrue('IDEAL' not in payment_methods, "IDEAL should not be in payment method list.")
+
+        # Test recurring restrictions.
+        payment_methods = factory.get_payment_methods(recurring=True)
+        self.assertTrue(len(payment_methods) == 0, "Payment method list should be empty.")
