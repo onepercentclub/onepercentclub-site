8a1b3bbd8ab0b28a69b9ebedc3d8e06cc33ac7e2
Ben Konrath <ben@1procentclub.nl>
2013-02-13 14:20:43 +0100

    Move donation / order item delete to donation view.

--- apps/fund/models.py
+++ apps/fund/models.py
@@ -34,12 +34,6 @@ class Donation(models.Model):
     def __unicode__(self):
         return str(self.id) + ' : ' + self.project.title + ' : EUR ' + str(self.amount)
 
-    # TODO: This needs to be re-implemented in the view.
-    # def delete(self, using=None):
-    #     # Tidy up! Delete related OrderItem, if any
-    #     OrderItem.objects.filter(object_id=self.id,content_type=ContentType.objects.get_for_model(Donation)).delete()
-    #     return super(Donation, self).delete()
-
 
 class Order(models.Model):
     """
