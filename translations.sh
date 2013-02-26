f432be0d5c054d1222387b1f14321d383544ef8a
Ben Konrath <ben@1procentclub.nl>
2013-02-26 18:13:59 +0100

    Update translations.

--- translations.sh
+++ translations.sh
@@ -6,15 +6,36 @@ MANAGE_PY="$PWD/manage.py"
 MAKEMESSAGES="$MANAGE_PY makemessages -l $SOURCE_LANGUAGE"
 COMPILEMESSAGES="$MANAGE_PY compilemessages"
 MINIMUM_PERC="80" # Don't pull in translations with less than this %
+APPS_DIR="apps"
+EXCLUDED_APPS=""
 
-APPS="accounts donations geo organizations projects"
+APPS=""
+for dir in $(find $APPS_DIR -maxdepth 1 -type d); do
+    if [ $dir = $APPS_DIR ]; then
+        continue
+    fi
+    app=$(echo $dir | sed -e "s|$APPS_DIR/||")
+    echo $EXCLUDED_APPS | grep -q $app
+    if [ $? -eq 0 ]; then
+        # $app is in $EXCLUDED_APPS list.
+        continue
+    fi
+
+    APPS="$APPS $dir"
+
+    # Make the locale dir if it's not there.
+    if [ ! -d "$dir/locale" ]; then
+        mkdir "$dir/locale"
+    fi
+done
+
+#APPS="accounts fund geo organizations projects wallposts reactions"
 
 case "$1" in
         generate)
             echo "Generating PO files"
-            for APP in $APPS; do
-                echo "Generating PO-file for $APP"
-                APP_DIR="apps/$APP"
+            for APP_DIR in $APPS; do
+                echo "Generating PO-file for $APP_DIR"
                 (cd $APP_DIR && $MAKEMESSAGES)
             done
 
@@ -37,9 +58,8 @@ case "$1" in
 
         compile)
             echo "Compiling PO files"
-            for APP in $APPS; do
-                echo "Compiling PO-file for $APP"
-                APP_DIR="apps/$APP"
+            for APP_DIR in $APPS; do
+                echo "Compiling PO-file for $APP_DIR"
                 (cd $APP_DIR && $COMPILEMESSAGES)
             done
 
@@ -51,5 +71,4 @@ case "$1" in
         *)
             echo $"Usage: $0 {generate|push|pull|compile}"
             exit 1
-
 esac
