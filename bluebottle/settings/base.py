746cead551513d3dfc7bfcf3279ae7221af17905
Ben Konrath <ben@1procentclub.nl>
2013-06-06 22:44:55 +0200

    Configure ALLOWED_HOSTS (new in Django 1.5).

--- bluebottle/settings/base.py
+++ bluebottle/settings/base.py
@@ -26,6 +26,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['.onepercentclub.com', '.1procentclub.nl']
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
