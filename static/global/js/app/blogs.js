a70c861bf5af0b7c05c2982e3c14e59b0e725ebe
Loek van Gent <loek@1procentclub.nl>
2013-07-16 22:29:37 +0200

    News with pagination. Fixes #BB-823

--- static/global/js/app/blogs.js
+++ static/global/js/app/blogs.js
@@ -18,6 +18,42 @@ App.NewsPreview = DS.Model.extend({
 
 /* Controller */
 
+App.NewsController = Em.ArrayController.extend({
+
+    page: 1,
+
+    // TODO: be smarter then this! Calculate hasNext.
+    hasNext: true,
+    hasPrevious: function(){
+        return this.get('page') > 1;
+    }.property('page'),
+
+
+
+    previousNews: function(){
+        var controller = this;
+        this.decrementProperty('page');
+        var page = this.get('page');
+        if (page > 0) {
+            var news  =  App.NewsPreview.find({language: App.get('language'), page: page});
+            news.one('didLoad', function(){
+                //controller.get('model').addObjects(news);
+                controller.set('model', news);
+            });
+        }
+    },
+    nextNews: function(){
+        var controller = this;
+        this.incrementProperty('page');
+        var page = this.get('page');
+        var news  =  App.NewsPreview.find({language: App.get('language'), page: page});
+        news.one('didLoad', function(){
+            //controller.get('model').addObjects(news);
+            controller.set('model', news);
+        });
+    }
+})
+
 
 /* Views */
 
