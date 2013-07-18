4e44c463cb0273bc806b3c4a4ddd2ef064bba3b7
Ben Konrath <ben@1procentclub.nl>
2013-07-18 09:55:03 +0200

    Add method and option to generate csv file for donations.

--- apps/bluebottle_salesforce/management/commands/sync_to_salesforce.py
+++ apps/bluebottle_salesforce/management/commands/sync_to_salesforce.py
@@ -1,9 +1,12 @@
+import csv
 import logging
+import os
 import sys
 from optparse import make_option
 from datetime import timedelta
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from django.conf import settings
 from apps.accounts.models import BlueBottleUser
 from apps.projects.models import Project, ProjectBudgetLine, ProjectCampaign, ProjectPitch, ProjectPlan
 from apps.organizations.models import Organization, OrganizationAddress
@@ -12,6 +15,7 @@ from apps.fund.models import Donation, Voucher, VoucherStatuses, DonationStatuse
 from apps.bluebottle_salesforce.models import (SalesforceOrganization, SalesforceContact, SalesforceProject,
                                                SalesforceDonation, SalesforceProjectBudget, SalesforceTask,
                                                SalesforceTaskMembers, SalesforceVoucher)
+
 
 logger = logging.getLogger(__name__)
 error_count = 0
@@ -41,7 +45,10 @@ class Command(BaseCommand):
                     help="Only sync records that have been updated in the last MINUTES minutes."),
 
         make_option('--sync-all', action='store_true', dest='sync_all',
-                    help="Sync all records.")
+                    help="Sync all records."),
+
+        make_option('--generate-csv-files', action='store_true', dest='generate_csv_files', default=False,
+                    help="Generate CSV files instead of syncing data with the Salesforce REST API.")
     )
 
     def handle(self, *args, **options):
@@ -52,7 +59,7 @@ class Command(BaseCommand):
         if options['sync_updated'] and options['sync_all']:
             logger.error("You cannot set both '--sync-all' and '--sync-updated'.")
             sys.exit(1)
-        elif not options['sync_updated'] and not options['sync_all']:
+        elif not options['generate_csv_files'] and not options['sync_updated'] and not options['sync_all']:
             logger.error("You must set either '--sync-all' or '--sync-updated MINUTES'. See help for more information.")
             sys.exit(1)
 
@@ -62,15 +69,18 @@ class Command(BaseCommand):
             sync_from_datetime = timezone.now() - delta
             logger.info("Syncing only updated records from {0}".format(timezone.localtime(sync_from_datetime)))
 
-        # The synchronization methods need to be run in a specific order because of foreign key dependencies.
-        sync_organizations(options['dry_run'], sync_from_datetime)
-        sync_users(options['dry_run'], sync_from_datetime)
-        sync_projects(options['dry_run'], sync_from_datetime)
-        sync_budget_lines(options['dry_run'], sync_from_datetime)
-        sync_tasks(options['dry_run'], sync_from_datetime)
-        sync_task_members(options['dry_run'], sync_from_datetime)
-        sync_donations(options['dry_run'], sync_from_datetime)
-        sync_vouchers(options['dry_run'], sync_from_datetime)
+        if options['generate_csv_files']:
+            generate_donations_csv_file()
+        else:
+            # The synchronization methods need to be run in a specific order because of foreign key dependencies.
+            sync_organizations(options['dry_run'], sync_from_datetime)
+            sync_users(options['dry_run'], sync_from_datetime)
+            sync_projects(options['dry_run'], sync_from_datetime)
+            sync_budget_lines(options['dry_run'], sync_from_datetime)
+            sync_tasks(options['dry_run'], sync_from_datetime)
+            sync_task_members(options['dry_run'], sync_from_datetime)
+            sync_donations(options['dry_run'], sync_from_datetime)
+            sync_vouchers(options['dry_run'], sync_from_datetime)
 
         logger.info("Synchronization finished with {0} successes and {1} errors.".format(success_count, error_count))
 
@@ -507,6 +517,48 @@ def sync_donations(dry_run, sync_from_datetime):
         if not dry_run:
             try:
                 sfdonation.save()
+                success_count += 1
+            except Exception as e:
+                error_count += 1
+                logger.error("Error while saving donation id {0}: ".format(donation.id) + str(e))
+
+
+def generate_donations_csv_file():
+    global error_count
+    global success_count
+
+    filename = 'donations-{0}.csv'.format(timezone.now().strftime('%Y-%m-%dT%H:%M:%S'))
+    with open(os.path.join(settings.PROJECT_ROOT, filename), 'wb') as csv_outfile:
+        csvwriter = csv.writer(csv_outfile, quoting=csv.QUOTE_ALL)
+
+        # TODO get field names from model for csv header.
+        # SalesforceDonation._meta.fields
+
+        csvwriter.writerow(["RECEIVER", "AMOUNT", "CLOSEDATE", "ID", "NAME", "ACCOUNTID",
+                            "PAYMENT_METHOD__C", "PROJECT__C", "STAGENAME", "TYPE__C", "DONATION_CREATED_DATE__C"])
+
+        donations = Donation.objects.all()
+        for donation in donations:
+            try:
+                receiver_id = ''
+                if donation.user:
+                    receiver_id = donation.user.id
+
+                project_id = ''
+                if donation.project:
+                    project_id = donation.project.id
+
+                csvwriter.writerow([receiver_id,                                    # receiver
+                                    '%01.2f' % (float(donation.amount) / 100),      # amount
+                                    donation.created.date(),                        # closedate
+                                    '',                                             # id
+                                    '',                                             # name
+                                    '',                                             # accountid
+                                    '',                                             # payment_method_c
+                                    project_id,                                     # project_c
+                                    DonationStatuses.values[donation.status],       # stagename
+                                    '',                                             # type_c
+                                    donation.created.date()])                       # donation_create_date_c
                 success_count += 1
             except Exception as e:
                 error_count += 1
