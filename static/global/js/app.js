796baf1d2bf3d2942c08174f675ea00ba188c003
Ben Konrath <ben@1procentclub.nl>
2013-07-25 16:53:14 +0200

    Add App.UserOrdersRoute.

--- static/global/js/app.js
+++ static/global/js/app.js
@@ -268,8 +268,6 @@ App.Adapter = DS.DRF2Adapter.extend({
         "organizations/documents/manage": "organizations/documents/manage",
         "projects/ambassadors/manage": "projects/ambassadors/manage",
         "projects/budgetlines/manage": "projects/budgetlines/manage",
-        "fund/paymentinfo": "fund/paymentinfo",
-        "fund/paymentmethodinfo": "fund/paymentmethodinfo",
         "users/activate": "users/activate",
         "users/passwordset": "users/passwordset",
         "homepage": "homepage",
@@ -1262,6 +1260,35 @@ App.UserSettingsRoute = Em.Route.extend({
     exit: function() {
         this._super();
         this.controllerFor('userSettings').stopEditing();
+    }
+});
+
+
+App.UserOrdersRoute = Em.Route.extend({
+    model: function(params) {
+        return App.Order.find({status: 'closed'});
+    },
+
+    setupController: function(controller, closedOrders) {
+        this._super(controller, closedOrders);
+
+        // Set the monthly order.
+        App.Order.find({status: 'recurring'}).then(function(recurringOrders) {
+            if (recurringOrders.get('length') > 0) {
+                controller.set('recurringOrder', recurringOrders.objectAt(0))
+            } else {
+                controller.set('recurringOrder', null)
+            }
+        });
+
+        // Set the payment.
+        App.RecurringDirectDebitPayment.find({}).then(function(recurringPayments) {
+            if (recurringPayments.get('length') > 0) {
+                controller.set('recurringPayment', recurringPayments.objectAt(0));
+            } else {
+                controller.set('recurringPayment', null);
+            }
+        });
     }
 });
 
