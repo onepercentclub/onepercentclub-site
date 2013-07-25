900b318b1b7e4c5c2a5b6f4c5a077834fe8028a3
Loek van Gent <loek@1procentclub.nl>
2013-07-25 21:10:11 +0200

    Fix task list to task view

--- static/global/js/app.js
+++ static/global/js/app.js
@@ -662,9 +662,11 @@ App.ApplicationRoute = Em.Route.extend({
         },
         showTask: function(task) {
             var route = this;
-            App.Project.find(task.get('project.id')).then(function(project) {
-                route.transitionTo('project', project);
-                route.transitionTo('projectTask', task);
+            App.Task.find(task.get('id')).then(function(task) {
+                App.Project.find(task.get('project.id')).then(function(project) {
+                    route.transitionTo('project', project);
+                    route.transitionTo('projectTask', task);
+                });
             });
         },
         showNews: function(news_id) {
