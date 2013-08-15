2ec89edf52b36228497bc348e20933dbf2a4ec18
Joeri Bekker <joeri@maykinmedia.nl>
2013-08-15 16:24:13 +0200

    Optimized query, added fields to list view and added partner to search query.

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -2,6 +2,7 @@ import logging
 from apps.projects.models import ProjectPlan, ProjectCampaign, ProjectTheme, ProjectPhases
 from django.core.urlresolvers import reverse
 from django.http.response import HttpResponseRedirect
+from django.utils.html import escape
 from django.utils.translation import ugettext_lazy as _
 
 logger = logging.getLogger(__name__)
@@ -136,15 +137,42 @@ class ProjectAdmin(AdminImageMixin, admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
     list_filter = ('phase', 'partner_organization')
-    list_display = ('title', 'owner', 'coach', 'phase', 'created')
+    list_display = ('get_title_display', 'get_owner_display', 'coach', 'phase', 'funded', 'created')
 
-    search_fields = ('title', 'owner__first_name', 'owner__last_name')
+    search_fields = ('title', 'owner__first_name', 'owner__last_name', 'partner_organization__name')
 
     raw_id_fields = ('owner', 'coach')
 
     readonly_fields = ('project_owner', 'pitch_view', 'plan_view', 'campaign_view')
 
     fields = readonly_fields + ('owner', 'coach', 'title', 'slug', 'phase', 'partner_organization')
+
+    def queryset(self, request):
+        # Optimization: Select related fields that are used in admin specific display fields.
+        queryset = super(ProjectAdmin, self).queryset(request)
+        return queryset.select_related('projectpitch', 'projectplan', 'projectcampaign', 'owner', 'partner_organization')
+
+    def get_title_display(self, obj):
+        if len(obj.title) > 50:
+            return u'<span title="{title}">{short_title} [...]</span>'.format(title=escape(obj.title), short_title=obj.title[:45])
+        return obj.title
+    get_title_display.allow_tags = True
+    get_title_display.admin_order_field  = 'title'
+    get_title_display.short_description = _('title')
+
+    def get_owner_display(self, obj):
+        owner_name = obj.owner.get_full_name()
+        if owner_name:
+            owner_name = u' ({name})'.format(name=owner_name)
+        return u'{email}{name}'.format(name=owner_name, email=obj.owner.email)
+    get_owner_display.admin_order_field  = 'owner__last_name'
+    get_owner_display.short_description = _('owner')
+
+    def funded(self, obj):
+        try:
+            return u'{funded} %'.format(funded=obj.projectcampaign.percentage_funded)
+        except ProjectCampaign.DoesNotExist:
+            return _('n/a')
 
     def project_owner(self, obj):
         object = obj.owner
