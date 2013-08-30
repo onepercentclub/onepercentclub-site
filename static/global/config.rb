37fd5317cec1d46dde424ab62ff166e2aefcc8f1
Frans Twisk <frans@franstwisk.nl>
2013-08-30 15:16:19 +0200

    First global restructure

--- static/global/config.rb
+++ static/global/config.rb
@@ -9,13 +9,13 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :compressed # by Compass.app 
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+line_comments = true
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
@@ -25,3 +25,4 @@ line_comments = false
 preferred_syntax = :scss
 
 sass_options = {:debug_info=>true} # by Compass.app
+
