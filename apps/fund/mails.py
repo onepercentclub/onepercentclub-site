a2fbc0b4484fc18eef44d00ea39aa61fc4af138a
Ben Konrath <ben@1procentclub.nl>
2013-09-17 13:55:52 +0200

    Move monthly donations process script to its own app.

--- apps/fund/mails.py
+++ apps/fund/mails.py
@@ -59,21 +59,3 @@ def mail_custom_voucher_request(voucher_request, *args, **kwargs):
                                  to=[system_email])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
-
-@task
-def mail_monthly_donation_processed_notification(recurring_payment, recurring_order):
-    site = 'https://' + Site.objects.get_current().domain
-    receiver = recurring_payment.user
-
-    # Compose the mail
-    context = Context({'order': recurring_order,
-                       'receiver_first_name': receiver.first_name.capitalize(),
-                       'date': format_date(locale='nl_NL'),
-                       'amount': format_currency(recurring_payment.amount / 100, 'EUR', locale='nl_NL'),
-                       'site': site})
-    subject = "Bedankt voor je maandelijkse support"
-    text_content = get_template('monthly_donation.mail.txt').render(context)
-    html_content = get_template('monthly_donation.mail.html').render(context)
-    msg = EmailMultiAlternatives(subject=subject, body=text_content, to=[receiver.email])
-    msg.attach_alternative(html_content, "text/html")
-    msg.send()
--- apps/recurring_donations/management/commands/process_monthly_donations.py
+++ apps/recurring_donations/management/commands/process_monthly_donations.py
@@ -16,9 +16,10 @@ from django.utils import timezone
 from apps.cowry_docdata.adapters import WebDirectDocDataDirectDebitPaymentAdapter
 from apps.cowry_docdata.exceptions import DocDataPaymentException
 from apps.cowry_docdata.models import DocDataPaymentOrder
+from apps.fund.models import RecurringDirectDebitPayment, Order, OrderStatuses, Donation, OrderItem
 from apps.projects.models import Project, ProjectPhases
 from ...mails import mail_monthly_donation_processed_notification
-from ...models import RecurringDirectDebitPayment, Order, OrderStatuses, Donation, OrderItem
+
 
 logger = logging.getLogger(__name__)
 
