4106880d639f58080ff4472a07cd770462863242
Loek van Gent <loek@1procentclub.nl>
2013-10-03 11:03:20 +0200

    Better display for organization documents in admin

--- static/global/js/app/manage.js
+++ static/global/js/app/manage.js
@@ -698,7 +698,7 @@ App.MyProjectPlanSubmitController = Em.ObjectController.extend(App.Editable, {
         model.on('didUpdate', function(){
             controller.transitionToRoute('myProjectPlanReview');
         });
-        this.updateRecordOnServer();
+        model.save();
     },
     exit: function(){
         this.set('model.status', 'new');
