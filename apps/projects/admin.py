73882b6e2a93a80fdd34a15841d81827518d5a08
Sergei Maertens <sergei@maykinmedia.nl>
2013-10-30 15:34:52 +0100

    Date since fully funded was in wrong place

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -200,7 +200,7 @@ class ProjectAdmin(AdminImageMixin, admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
     list_filter = ('phase', 'partner_organization')
-    list_display = ('get_title_display', 'get_owner_display', 'coach', 'get_phase_display', 'funded', 'created')
+    list_display = ('get_title_display', 'get_owner_display', 'coach', 'phase', 'funded', 'created')
 
     search_fields = ('title', 'owner__first_name', 'owner__last_name', 'partner_organization__name')
 
@@ -235,24 +235,18 @@ class ProjectAdmin(AdminImageMixin, admin.ModelAdmin):
     get_owner_display.admin_order_field = 'owner__last_name'
     get_owner_display.short_description = _('owner')
 
-    def get_phase_display(self, obj):
-        phase = obj.get_phase_display()
-        if obj.phase == 'act':
-            # add the date when this phase was reached
-            try:
-                phase_log = obj.projectphaselog_set.get(phase=obj.phase)
-                created = formats.date_format(phase_log.created, 'SHORT_DATETIME_FORMAT')
-                phase = _(u'{phase} (since {created})').format(phase=phase, created=created)
-            except ProjectPhaseLog.DoesNotExist:
-                pass
-        return phase
-
-    get_phase_display.admin_order_field = 'phase'
-    get_phase_display.short_description = _('phase')
-
     def funded(self, obj):
         try:
-            return u'{funded} %'.format(funded=obj.projectcampaign.percentage_funded)
+            # 
+            funded = obj.projectcampaign.percentage_funded
+            if funded == 100.0:
+                try:
+                    phase_log = obj.projectphaselog_set.get(phase=ProjectPhases.act)
+                    created = formats.date_format(phase_log.created, 'SHORT_DATETIME_FORMAT')
+                    return u'{funded} % ({date})'.format(funded=funded, date=created)
+                except ProjectPhaseLog.DoesNotExist:
+                    pass
+            return u'{funded} %'.format(funded=funded)
         except ProjectCampaign.DoesNotExist:
             return _('n/a')
 
