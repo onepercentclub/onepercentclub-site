5a960a97e8dd8b29f68eb14e0b2319a649a80c4d
Mark Gallop <mark@rollick.id.au>
2013-11-03 02:32:13 +0100

    try to get js tests working with travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     karma: {
+      ci: { configFile: 'test/js/config/karma.conf.js', singleRun: true, browsers: ['PhantomJS'] },
       unit: { configFile: 'test/js/config/karma.conf.js', keepalive: true },
       chrome: { configFile: 'test/js/config/karma.conf.js', keepalive: true, browsers: ['Chrome']  }
       // e2e: { configFile: 'test/js/config/e2e.js', keepalive: true }, // End-to-end / Functional Tests
