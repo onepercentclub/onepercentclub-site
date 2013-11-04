0489ea36c27a2c8ec2dcfeb523e53697ba04799a
Sergei Maertens <sergei@maykinmedia.nl>
2013-11-04 15:09:11 +0100

    removed field registration from admin, removed obsolete form/widget

--- apps/organizations/forms.py
+++ apps/organizations/forms.py
@@ -23,41 +23,6 @@ class UploadWidget(forms.FileInput):
         )
         return html
 
-class PrivateUrlFileInput(AdminFileWidget):
-    """ 
-    Class to allow private file downloads, changing the url to a view serving
-    the file.
-    """
-    
-    is_required = False
-    
-    def render(self, name, value, attrs=None):
-        """ Override render to modify the download url """
-        substitutions = {
-            'initial_text': self.initial_text,
-            'input_text': self.input_text,
-            'clear_template': '',
-            'clear_checkbox_label': self.clear_checkbox_label,
-        }
-        template = '%(input)s'
-        substitutions['input'] = super(forms.ClearableFileInput, self).render(name, value, attrs)
-
-        if value and hasattr(value, "url"):
-            url = reverse('organization-registration-download', kwargs={'pk': self.instance.id})
-            template = self.template_with_initial
-            substitutions['initial'] = format_html('<a href="{0}">{1}</a>',
-                                                   url,
-                                                   force_text(value))
-            if not self.is_required:
-                checkbox_name = self.clear_checkbox_name(name)
-                checkbox_id = self.clear_checkbox_id(checkbox_name)
-                substitutions['clear_checkbox_name'] = conditional_escape(checkbox_name)
-                substitutions['clear_checkbox_id'] = conditional_escape(checkbox_id)
-                substitutions['clear'] = forms.CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
-                substitutions['clear_template'] = self.template_with_clear % substitutions
-
-        return mark_safe(template % substitutions)
-
 
 # Forms
 class OrganizationDocumentForm(forms.ModelForm):
@@ -70,14 +35,3 @@ class OrganizationDocumentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(OrganizationDocumentForm, self).__init__(*args, **kwargs)
         self.fields['file'].required = False
-
-class OrganizationForm(forms.ModelForm):
-    class Meta:
-        model = Organization
-        widgets = {
-            'registration': PrivateUrlFileInput()
-        }
-
-    def __init__(self, *args, **kwargs):
-        super(OrganizationForm, self).__init__(*args, **kwargs)
-        self.fields['registration'].widget.instance = self.instance
