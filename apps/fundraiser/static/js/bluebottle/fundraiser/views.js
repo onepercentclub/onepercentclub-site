84e310d7e28eea2e65738ce70302422b925f9d86
Joeri Bekker <joeri@maykinmedia.nl>
2013-11-19 10:32:51 +0100

    Initial create campaign form template and Ember routes, views and controllers.

---
+++ apps/fundraiser/static/js/bluebottle/fundraiser/views.js
@@ -0,0 +1,19 @@
+App.FundRaiserView = Em.View.extend({
+    templateName: 'fund_raiser'
+
+});
+
+
+App.MyFundRaiserListView = Em.View.extend({
+    templateName: 'my_fund_raiser_list'
+});
+
+
+App.MyFundRaiserNewView = Em.View.extend({
+    templateName: 'my_fund_raiser_new'
+});
+
+
+App.MyFundRaiserView = Em.View.extend({
+    templateName: 'my_fund_raiser'
+});
