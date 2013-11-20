15c1bc99a947d25e501b41732114956fb77eca60
Sergei Maertens <sergei@maykinmedia.nl>
2013-11-20 15:26:41 +0100

    Renamed fundraiser app to fundraisers Adapted migration files to reflect this change Added dependency for Donation model migration Modified settings to include correct app (fundraisers vs. fundraiser)

--- apps/fund/migrations/0014_auto__add_field_donation_fundraiser.py
+++ apps/fund/migrations/0014_auto__add_field_donation_fundraiser.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("fundraisers", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding field 'Donation.fundraiser'
         db.add_column(u'fund_donation', 'fundraiser',
