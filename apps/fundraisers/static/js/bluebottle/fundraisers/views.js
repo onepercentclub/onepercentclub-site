7238934d3d17d42919c45ea5495f4f940ed0ce44
Loek van Gent <loek@1procentclub.nl>
2013-11-28 16:13:02 +0100

    Fundraiser popup with image

--- apps/fundraisers/static/js/bluebottle/fundraisers/views.js
+++ apps/fundraisers/static/js/bluebottle/fundraisers/views.js
@@ -24,20 +24,31 @@ App.ProjectFundRaiserPopupView = Em.View.extend({
 App.ProjectFundRaiserView = Em.View.extend({
 	templateName: 'project_fundraiser',
 
+    html: function(){
+        /*
+        var controller = this.get('controller');
+        var contentView = App.ProjectFundRaiserPopupView.create({controller: controller});
+        return contentView.renderToBuffer().buffer;
+        */
+        var model= this.get('controller');
+        return '<h3>' + model.get('model.owner.full_name') + '</h3>' +
+                '<img src="' + model.get('model.image.small') +  '" />' +
+                '<div class="project-fund-amount">' +
+                '<strong class="amount-donated">&euro;' + model.get('model.amount_donated') + '</strong> ' +
+                gettext('of') + ' <strong class="amount-asked">&euro;' + model.get('model.amount') + '</strong> ' +
+                gettext('raised') + '</div>';
+
+    }.property('controller.title'),
+
     tagName: 'li',
     didInsertElement: function(){
         var model = this.get('controller.content');
-        var controller = this.get('controller');
-        this.$('span').popover({
+        this.$('img').popover({
             trigger: 'hover',
             placement: 'top',
             title: model.get('title'),
-            html : true,
-            content : function(el){
-                var contentView = App.ProjectFundRaiserPopupView.create({controller: controller});
-                var html = contentView.renderToBuffer().buffer;
-                return html;
-            }
+            html: true,
+            content: this.get('html')
         });
     }
 })
