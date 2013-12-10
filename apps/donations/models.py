b627d455f7020da0082836002f4b3bc54fc540b4
Loek van Gent <loek@1procentclub.nl>
2013-12-10 10:21:22 +0100

    Fix mails for one-off donations

--- apps/donations/models.py
+++ apps/donations/models.py
@@ -1,10 +1,9 @@
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
-from apps.fund.models import Order, OrderStatuses, Donation, DonationStatuses, RecurringDirectDebitPayment
+from apps.fund.models import Order, OrderStatuses, RecurringDirectDebitPayment
 from apps.projects.models import Project
 from apps.projects.signals import project_funded
 
-from .mails import mail_project_funded_monthly_donor_notification, mail_new_oneoff_donation
+from .mails import *
 
 
 # The monthly donor email will be sent under these conditions:
@@ -40,25 +39,3 @@ def email_monthly_donor_project_funded(sender, instance, first_time_funded, **kw
                             mail_project_funded_monthly_donor_notification(donation.user, instance)
 
 
-@receiver(pre_save, sender=Donation, dispatch_uid="new_oneoff_donation")
-def new_oneoff_donation(sender, instance, **kwargs):
-    """
-    Send project owner a mail if a new "one off" donation is done. We consider a donation done if the status is pending.
-    """
-    # Only process the donation if it is of type "one off".
-    if instance.donation_type != Donation.DonationTypes.one_off:
-        return
-
-    # If the instance has no PK the previous status is unknown.
-    if instance.pk:
-        # NOTE: We cannot check the previous and future state of the ready attribute since it is set in the
-        # Donation.save function.
-
-        existing_donation = Donation.objects.get(pk=instance.pk)
-        # If the existing donation is already pending, don't mail.
-        if existing_donation.status == DonationStatuses.pending:
-            return
-
-    # If the donation status will be pending, send a mail.
-    if instance.status == DonationStatuses.pending:
-        mail_new_oneoff_donation(instance)
