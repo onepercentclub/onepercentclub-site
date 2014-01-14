afca2021b78ed47aff30a6178e81c54768500f9c
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-14 14:57:24 +0100

    Basic test creating a campaign.
    Uses django-dynamic-fixture for automating object
    creation.

---
+++ apps/payouts/tests.py
@@ -0,0 +1,73 @@
+from django.test import TestCase
+
+from django_dynamic_fixture import N, G
+
+from apps.projects.models import (
+    Project, ProjectPhases, ProjectCampaign, ProjectPlan
+)
+from apps.fund.models import Donation, DonationStatuses
+from .models import Payout
+
+
+class PayoutTestCase(TestCase):
+    """ Testcase for Payouts. """
+
+    def setUp(self):
+        """ Setup a project ready for payout. """
+        self.project = G(
+            Project
+        )
+
+        self.campaign = G(
+            ProjectCampaign,
+            project=self.project,
+            money_asked=1500
+        )
+
+        self.projectplan = G(
+            ProjectPlan,
+            project=self.project
+        )
+
+        # Update phase to campaign.
+        self.project.phase = ProjectPhases.campaign
+        self.project.save()
+
+        self.donation = G(
+            Donation,
+            project=self.project,
+            voucher=None,
+            donation_type=Donation.DonationTypes.one_off,
+            amount=1500
+        )
+
+    def test_save(self):
+        """ Test saving a payout. """
+
+        # Generate new payout
+        payout = N(Payout)
+
+        # Save it
+        payout.save()
+
+    def test_create_payout(self):
+        """
+        Test automatically generating a payout.
+        """
+
+        # No payouts should exist yet as project is not in act phase yet
+        self.assertFalse(Payout.objects.exists())
+
+        # Update phase to act.
+        self.project.phase = ProjectPhases.act
+        self.project.save()
+
+        # Set status of donation to paid
+        self.donation.status = DonationStatuses.paid
+        self.donation.save()
+
+        # Payout should have been created
+        self.assertEquals(Payout.objects.count(), 1)
+
+        payout = Payout.objects.all()[0]
+        self.assertEquals(payout.project, self.project)
