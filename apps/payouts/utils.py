2897f10b016a0f12b6428e63a7dbe38308e92ee5
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-15 15:10:22 +0100

    Setting and functions for calculating VAT.

--- apps/payouts/utils.py
+++ apps/payouts/utils.py
@@ -1,6 +1,10 @@
 import decimal
 
+from django.conf import settings
+
 from .choices import PayoutRules
+
+VAT_RATE = decimal.Decimal(settings.VAT_RATE)
 
 
 def money_from_cents(amount):
@@ -20,7 +24,7 @@ def round_money(amount):
 
     assert isinstance(amount, decimal.Decimal)
 
-    return amount.quantize(decimal.Decimal('.01'), rounding=decimal.ROUND_DOWN)
+    return amount.quantize(decimal.Decimal('.01'), rounding=decimal.ROUND_HALF_DOWN)
 
 
 def get_fee_percentage(rule):
@@ -40,3 +44,37 @@ def get_fee_percentage(rule):
 
     # Other
     raise NotImplementedError('Payment rule not implemented yet.')
+
+def calculate_vat(amount):
+    """
+    Calculate VAT over exclusive amount according to default percentage.
+
+    >>> calculate_vat(decimal.Decimal('10'))
+    Decimal('2.10')
+    """
+
+    return round_money(amount * VAT_RATE)
+
+def calculate_vat_inclusive(amount):
+    """
+    Calculate the inclusive amount for amounts excluding VAT.
+
+    >>> calculate_vat_inclusive(decimal.Decimal('10'))
+    Decimal('12.10')
+    """
+
+    factor = VAT_RATE + decimal.Decimal('1.00')
+
+    return round_money(amount * factor)
+
+def calculate_vat_exclusive(amount):
+    """
+    Calculate the exclusive amont for amounts including VAT.
+
+    >>> calculate_vat_exclusive(decimal.Decimal('12.10'))
+    Decimal('10.00')
+    """
+
+    factor = VAT_RATE + decimal.Decimal('1.00')
+
+    return round_money(amount / factor)
