d7d93453861a7d4b66fdd22d01a64892eecda626
Loek van Gent <loek@1procentclub.nl>
2014-02-12 15:45:48 +0100

    Remove obsolete stuff add 1% JS additional

--- apps/onepercent_projects/static/js/projects/models.js
+++ apps/onepercent_projects/static/js/projects/models.js
@@ -1,5 +1,7 @@
 App.Project.reopen({
 
+    isFundable: Em.computed.equal('status', 6),
+
     isStatusPlan: Em.computed.equal('status.id', 5),
     isStatusCampaign: Em.computed.equal('status.id', 6),
     isStatusCompleted: Em.computed.equal('status.id', 8)
