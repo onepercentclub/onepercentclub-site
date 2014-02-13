62b89610627673854721601812d0131206b10e44
Loek van Gent <loek@1procentclub.nl>
2014-02-13 21:57:41 +0100

    Change travis settings for pip install

--- apps/members/models.py
+++ apps/members/models.py
@@ -1,8 +1,12 @@
+from bluebottle.utils.models import Address
 from django.db import models
 from bluebottle.bb_accounts.models import BlueBottleBaseUser
 
+
 class Member(BlueBottleBaseUser):
+
+    address = models.ForeignKey('UserAddress')
+
+
+class UserAddress(Address):
     pass
-
-
-
