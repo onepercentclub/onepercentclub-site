bae76d469e5b04429bb2b1c99c6c60e4992777cf
Mark Gallop <mark@rollick.id.au>
2014-04-02 00:16:07 +0200

    move project api url definitions to use same structure as in BB add comment for old(??) url definitions

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,3 +1,4 @@
+# TODO: Do we still need this?
 from django.conf.urls import patterns
 from surlex.dj import surl
 from .views import ProjectDetailView, ProjectIframeView
@@ -5,5 +6,4 @@ from .views import ProjectDetailView, ProjectIframeView
 urlpatterns = patterns('',
     surl(r'^<slug:s>$', ProjectDetailView.as_view(), name='project-detail'),
     surl(r'^<slug:s>/iframe/$', ProjectIframeView.as_view(), name='project-iframe'),
-    #surl('/macromicro/xml', MacroMicroListView.as_view(), name='macromicro-project-list')
 )
