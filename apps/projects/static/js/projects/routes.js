a7b5fa3af43f0269bcc9264a939bc9357e880217
Marco Giannone <marco.giannone@gmail.com>
2014-04-09 14:46:46 +0200

    CLUB-116, CLUB-111 added goal page and added crowdfunding goal, moved deadline from pitch to goal page

---
+++ apps/projects/static/js/projects/routes.js
@@ -0,0 +1,7 @@
+App.Router.map(function(){
+    this.resource('myProject', {path: '/my/projects/:id'}, function() {
+		this.route('goal');
+    });
+});
+
+App.MyProjectGoalRoute = App.MyProjectSubRoute.extend({});
