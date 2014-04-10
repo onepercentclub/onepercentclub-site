f1baef649fd38c951237120febf05c616e0d00b9
Mark Gallop <mark@rollick.id.au>
2014-04-10 16:55:55 +0200

    fix spacing

--- apps/projects/static/js/projects/models.js
+++ apps/projects/static/js/projects/models.js
@@ -1,17 +1,17 @@
 App.Project.reopen({
-	url: "projects",
+    url: "projects",
 
-	deadline: DS.attr('date'),
+    deadline: DS.attr('date'),
     amount_asked: DS.attr('number'),
 
-	maxAmountAsked: Ember.computed.lte('amount_asked', 1000000),
-	minAmountAsked: Ember.computed.gte('amount_asked', 250),
+    maxAmountAsked: Ember.computed.lte('amount_asked', 1000000),
+    minAmountAsked: Ember.computed.gte('amount_asked', 250),
 
     amount_donated: DS.attr('number'),
 
-	amount_needed: function() {
-		return this.get('amount_asked') - this.get('amount_donated');
-	}.property('amount_asked', 'amount_donated'),
+    amount_needed: function() {
+		    return this.get('amount_asked') - this.get('amount_donated');
+    }.property('amount_asked', 'amount_donated'),
 
     task_count: DS.attr('number'),
 
@@ -19,7 +19,6 @@ App.Project.reopen({
 
     isStatusCampaign: Em.computed.equal('status.id', '5'),
     isStatusCompleted: Em.computed.equal('status.id', '7')
-
 });
 
 App.ProjectPreview.reopen({
@@ -30,19 +29,19 @@ App.MyProject.reopen({
     image: DS.attr('image'),
     video: DS.attr('string'),
 
-	init: function () {
+    init: function () {
         this._super();
 
         this.validatedFieldsProperty('validGoal', this.get('requiredGoalFields'));
         this.missingFieldsProperty('missingFieldsGoal', this.get('requiredGoalFields'));
-	},
+    },
 
-	valid: function(){
+    valid: function(){
         return (this.get('') && this.get('validPitch') && this.get('validGoal'));
     }.property('validStory', 'validPitch', 'validGoal'),
 
     requiredStoryFields: ['description', 'goal', 'destination_impact'],
-	requiredGoalFields: ['amount_asked', 'deadline', 'maxAmountAsked', 'minAmountAsked'],
+    requiredGoalFields: ['amount_asked', 'deadline', 'maxAmountAsked', 'minAmountAsked'],
     requiredPitchFields: ['title', 'pitch', 'image', 'theme', 'tags.length', 'country', 'latitude', 'longitude'],
 
     friendlyFieldNames: {
