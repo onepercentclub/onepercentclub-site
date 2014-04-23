9a58d60614dd72ff0f5640d44ed183a47b301b78
Loek van Gent <loek@1procentclub.nl>
2014-04-24 00:01:40 +0200

    Remove old apps

--- apps/hide_banners/urlsapi.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from surlex.dj import surl
-from .views import SlideList
-
-urlpatterns = patterns('',
-    url(r'^$', SlideList.as_view(), name='slide-list'),
-)
