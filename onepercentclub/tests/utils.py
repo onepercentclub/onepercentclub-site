5fc26f88e92b26f87e6b10e77c69afd2b11f0f35
Mark Gallop <mark@rollick.id.au>
2014-04-25 13:41:44 +0200

    refactor tests a little to use selenium waits rather than sleep and bump BB version

--- onepercentclub/tests/utils.py
+++ onepercentclub/tests/utils.py
@@ -57,32 +57,4 @@ class OnePercentSeleniumTestCase(SeleniumTestCase):
         self.visit_path('', lang_code)
 
         # # Check if the homepage opened, and the dynamically loaded content appeared.
-        # # Remember that
-        return self.browser.is_text_present('CHOOSE YOUR PROJECT', wait_time=10)
-
-    def assertDatePicked(self):
-        # Pick a deadline next month
-        self.assertTrue(self.scroll_to_and_click_by_css(".hasDatepicker"))
-
-        # Wait for date picker popup
-        self.assertTrue(self.browser.is_element_present_by_css("#ui-datepicker-div"))
-
-        # Click Next to get a date in the future
-        self.browser.find_by_css("[title=Next]").first.click()
-        self.assertTrue(self.browser.is_text_present("10"))
-        self.browser.find_link_by_text("10").first.click()
-
-    def scroll_to_and_click_by_css(self, selector):
-        if self.browser.is_element_present_by_css(selector):
-            element = self.browser.driver.find_element_by_css_selector(selector)
-            self.browser.execute_script("window.scrollTo(0,%s)" % element.location['y']);
-            element.click()
-            return True
-        else:
-            return False
-
-    def assert_css(self, selector, wait_time=5):
-        return self.assertTrue(self.browser.is_element_present_by_css(selector, wait_time=wait_time) )
-
-    def assert_text(self, text, wait_time=5):
-        return self.assertTrue(self.browser.is_text_present(text, wait_time=wait_time) )
+        return self.wait_for_element_css('#home')
