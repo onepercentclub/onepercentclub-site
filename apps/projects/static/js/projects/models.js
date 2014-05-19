34b8d5bbf4d5f085aa0f2f77170c76eed1234dcc
Loek van Gent <loek@1procentclub.nl>
2014-05-19 22:14:49 +0200

    REmove reference to removed phase 'done - stopped'

--- apps/projects/static/js/projects/models.js
+++ apps/projects/static/js/projects/models.js
@@ -23,7 +23,7 @@ App.Project.reopen({
     isStatusPlan: Em.computed.lt('status.id', '5'),
     isStatusCampaign: Em.computed.equal('status.id', '5'),
     isStatusCompleted: Em.computed.equal('status.id', '7'),
-    isStatusFinished: Em.computed.gt('status.id', '6'),
+    isStatusStopped: Em.computed.gt('status.id', '9'),
 
     save: function () {
         // the amount_needed is calculated here and not in the server
