df6b72bbf4363d8a6ae3488f4379e45a3382e203
Jhon Majoor <jhon@onepercentclub.com>
2014-05-21 15:27:54 +0200

    scrolled header function

--- apps/core/static/js/bluebottle/app_overrides.js
+++ apps/core/static/js/bluebottle/app_overrides.js
@@ -1,3 +1,45 @@
 App.ApplicationController.reopen({
     needs: ['currentUser', 'currentOrder', 'myProjectList'],
 });
+
+
+App.Scrolling = Em.Mixin.create({
+
+	bindScrolling: function(opts) {
+		var onScroll, self = this;
+
+		onScroll = function() {
+			var scrollTop = $(this).scrollTop();
+			return self.scrolled(scrollTop);
+		};
+
+		$(window).bind('scroll', onScroll);
+		$(document).bind('touchmove', onScroll);
+	},
+
+	unbindScrolling: function () {
+		$(window).unbind('scroll');
+		$(document).unbind('touchmove');
+	}
+});
+
+App.ApplicationView.reopen(App.Scrolling, {
+	didInsertElement: function() {
+		this.bindScrolling();
+	},
+
+	willRemoveElement: function() {
+		this.unbindScrolling();
+	},
+
+	scrolled: function(dist) {
+		top = $('#content').offset();
+		elm = top.screen.availTop;
+
+		if (dist < elm) {
+			$('#header').removeClass('is-scrolled');
+		} else {
+			$('#header').addClass('is-scrolled');
+		}
+	}
+});
