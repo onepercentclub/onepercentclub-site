3df0490fb1d52b9f82feb89ace77096780285bdc
Mark Gallop <mark@rollick.id.au>
2014-05-22 15:24:48 +0200

    CLUB-465 (re-)add action to handle donations for fundraisers

--- apps/core/static/js/bluebottle/app_overrides.js
+++ apps/core/static/js/bluebottle/app_overrides.js
@@ -1,28 +1,52 @@
+/*
+  Bluebottle Route Overrides
+ */
+App.ApplicationRoute.reopen({
+    actions: {
+        addDonation: function (project, fundraiser) {
+            var route = this;
+            App.CurrentOrder.find('current').then(function(order) {
+                var store = route.get('store');
+
+                var projectHasDonation = order.get('donations').anyBy('project', project);
+                var fundraiserHasDonation = false;
+                if(fundraiser !== undefined){
+                    fundraiserHasDonation = order.get('donations').anyBy('fundraiser', fundraiser);
+                }
+
+                // TODO: functional test this.
+                // *  Donate directly to project: check if no direct donations exist
+                // *  Donate through fundraiser: check if donation for that fundraiser exists
+                // *  We can have the same project multiple times, but all different fundraisers
+                
+                if (fundraiserHasDonation ||
+                    (projectHasDonation && !fundraiserHasDonation && fundraiser === undefined)) {
+                    // Donation for this already exists in this order.
+                } else {
+                    var donation = store.createRecord(App.CurrentOrderDonation);
+                    donation.set('project', project);
+                    if(fundraiser !== undefined){
+                        donation.set('fundraiser', fundraiser);
+                    }
+                    donation.set('order', order);
+                    donation.save();
+                }
+                route.transitionTo('currentOrder.donationList');
+            });
+        }
+    }
+});
+
+/*
+  Bluebottle Controller Overrides
+ */
 App.ApplicationController.reopen({
     needs: ['currentUser', 'currentOrder', 'myProjectList'],
 });
 
-
-App.Scrolling = Em.Mixin.create({
-
-	bindScrolling: function(opts) {
-		var onScroll, self = this;
-
-		onScroll = function() {
-			var scrollTop = $(this).scrollTop();
-			return self.scrolled(scrollTop);
-		};
-
-		$(window).bind('scroll', onScroll);
-		$(document).bind('touchmove', onScroll);
-	},
-
-	unbindScrolling: function () {
-		$(window).unbind('scroll');
-		$(document).unbind('touchmove');
-	}
-});
-
+/*
+  Bluebottle View Overrides
+ */
 App.ApplicationView.reopen(App.Scrolling, {
 	setBindScrolling: function() {
 		this.bindScrolling();
@@ -44,4 +68,27 @@ App.ApplicationView.reopen(App.Scrolling, {
 			$('.nav-member-dropdown').addClass('is-scrolled');
 		}
 	}
+});
+
+/*
+  Some 1%Club Mixins
+ */
+App.Scrolling = Em.Mixin.create({
+
+  bindScrolling: function(opts) {
+    var onScroll, self = this;
+
+    onScroll = function() {
+      var scrollTop = $(this).scrollTop();
+      return self.scrolled(scrollTop);
+    };
+
+    $(window).bind('scroll', onScroll);
+    $(document).bind('touchmove', onScroll);
+  },
+
+  unbindScrolling: function () {
+    $(window).unbind('scroll');
+    $(document).unbind('touchmove');
+  }
 });
