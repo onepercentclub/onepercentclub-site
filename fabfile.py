b9252e68ad3d54da99832b401958555961ce401a
rollick <rollick@users.noreply.github.com>
2014-06-02 22:02:05 +0200

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -346,7 +346,6 @@ def prepare_django():
 
 
 def flush_memcache():
-    # FIXME: This doesn't appear to flush data - stop / start for now (don't trust restart)
     run('echo \'flush_all\' | nc -q1 localhost 11211')
 
 
