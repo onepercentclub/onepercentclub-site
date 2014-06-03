31404111a6d70244e5ab90473905b34f7cc17f39
Loek van Gent <loek@1procentclub.nl>
2014-06-03 12:24:14 +0200

    Fix for generating payout for fully funded projects

--- apps/payouts/signals.py
+++ apps/payouts/signals.py
@@ -16,7 +16,7 @@ def create_payout_finished_project(sender, instance, created, **kwargs):
     project = instance
     now = timezone.now()
 
-    if (project.status.name == ProjectPhase.objects.get(slug='done-complete') or
+    if (project.status == ProjectPhase.objects.get(slug='done-complete') or
             project.status == ProjectPhase.objects.get(slug='done-incomplete')) \
             and project.amount_asked:
 
