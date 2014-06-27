6315d400832db190fe6933785403464998c0c26d
Loek van Gent <loek@1procentclub.nl>
2014-06-27 10:28:54 +0200

    Upload screenshot in tests & some more optimisations

--- apps/projects/tests/test_functional.py
+++ apps/projects/tests/test_functional.py
@@ -237,12 +237,14 @@ class ProjectCreateSeleniumTests(OnePercentSeleniumTestCase):
         ###
 
         self.assertTrue(self.is_visible('section h1.page-title'))
-        self.scroll_to_and_click_by_css("button.btn-primary")
+        self.scroll_to_by_css("button.btn-primary, form")
+        time.sleep(5)
+        self.scroll_to_and_click_by_css("button.btn-primary, form")
 
         ###
         # Project Section
         ###
-
+        time.sleep(5)
 
         self.browser.select('language', 2)
         self.assertTrue(self.is_visible('input[name="title"]'))
@@ -440,7 +442,7 @@ class ProjectWallPostSeleniumTests(OnePercentSeleniumTestCase):
         """
         self.visit_path('/projects/{0}'.format(self.project.slug))
         #self.assertTrue(self.browser.is_text_present(self.project.title, wait_time=5))
-        self.assertTrue(self.browser.is_text_present('Post a new comment on wall', wait_time=5))
+        self.assertTrue(self.browser.is_text_present('Post a new comment', wait_time=5))
 
         self.browser.find_by_css(".wallpost-post-update").click()
         self.assertTrue(self.browser.is_text_present('Post', wait_time=5))
