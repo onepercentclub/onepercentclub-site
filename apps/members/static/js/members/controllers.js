b8bcca56c3b98a9501f850bd8d570af548a717e0
Mark Gallop <mark@rollick.id.au>
2014-07-03 15:11:27 +0200

    reset FB login errors when leaving modal

--- apps/members/static/js/members/controllers.js
+++ apps/members/static/js/members/controllers.js
@@ -1,3 +1,10 @@
 App.LoginController.reopen(App.AuthJwtMixin, {
-    loginTitle: gettext('Welcome')
+    loginTitle: gettext('Welcome'),
+
+    willClose: function () {
+        this._super();
+
+        // Also clear any FB login errors
+        FBApp.set('connectError', null);
+    }
 });
