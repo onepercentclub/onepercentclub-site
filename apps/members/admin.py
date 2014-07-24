070b505c0d1b881916eec7a679270b3032f11fce
Mark Gallop <mark@rollick.id.au>
2014-07-24 14:26:08 +0200

    add login as user for jwt token users CLUB-760 #resolve

--- apps/members/admin.py
+++ apps/members/admin.py
@@ -1,12 +1,37 @@
 from django.contrib import admin
+from django.conf.urls import patterns, url, include
+from django.http import HttpResponseRedirect
+
 
 from bluebottle.bb_accounts.admin import BlueBottleUserAdmin
 
 from .models import Member
 
+
 class MemberAdmin(BlueBottleUserAdmin):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(MemberAdmin, self).__init__(*args, **kwargs)
+        
+        self.list_display = ('email', 'first_name', 'last_name', 'is_staff', 'date_joined', 'is_active', 'login_as_link')
 
+    def get_urls(self):
+        urls = super(MemberAdmin, self).get_urls()
 
+        extra_urls = patterns('',
+                           (r'^login-as/(?P<user_id>\d+)/$',
+                            self.admin_site.admin_view(self.login_as_redirect)),
+                        )
+        return extra_urls + urls
 
-admin.site.register(Member, BlueBottleUserAdmin)
+    def login_as_redirect(self, *args, **kwargs):
+        user = Member.objects.get(id = kwargs.get('user_id', None))
+        url = "/#!/login-with/{0}".format(user.get_jwt_token())
+        
+        return HttpResponseRedirect(url)
+
+    def login_as_link(self, obj):
+        return "<a target='_blank' href='/admin/members/member/login-as/{0}/'>{1}</a>".format(obj.pk, 'Login as user')
+
+    login_as_link.allow_tags = True
+
+admin.site.register(Member, MemberAdmin)
