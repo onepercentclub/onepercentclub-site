1f5db00f50cd6a37e888d7f9c03d6177ae102fa5
Jhon Majoor <jhon@onepercentclub.com>
2014-07-25 12:21:12 +0200

    Changes after feedback Amber
    - changed style header video
    - removed quiz typo
    - revert dollar(s) to euro(s)
    - added a faq
    - tweaked responsive header

--- apps/partners/static/js/bluebottle/partners/controllers.js
+++ apps/partners/static/js/bluebottle/partners/controllers.js
@@ -9,7 +9,7 @@ App.CheetahQuizController = Em.Controller.extend({
 
     questionOnePassed: Em.computed.equal('questionOne', 'yes'),
     questionOneFailed: Em.computed.equal('questionOne', 'no'),
-    questionTwoFailed: Em.computed.gt('questionTwo', 20000),
+    questionTwoFailed: Em.computed.gt('questionTwo', 15000),
     questionThreePassed: Em.computed.equal('questionThree', 'yes'),
     questionThreeFailed: Em.computed.equal('questionThree', 'no'),
 
@@ -21,7 +21,7 @@ App.CheetahQuizController = Em.Controller.extend({
         removeComma = answer.split(','[0]);
         newAnswer = removeComma[0].replace('.', '');
         
-        return (newAnswer <= 20000);
+        return (newAnswer <= 15000);
     }.property('questionTwo'),
 
     actions: {
