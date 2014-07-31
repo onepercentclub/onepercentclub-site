0c350dad3cb97abee07a9afdc77e0162140fcf1b
Mark Gallop <mark@rollick.id.au>
2014-07-31 14:09:18 +0200

    add simple test for creating partner project

--- onepercentclub/tests/factory_models/project_factories.py
+++ onepercentclub/tests/factory_models/project_factories.py
@@ -3,14 +3,21 @@ from django.utils import timezone
 from bluebottle.test.factory_models.projects import ProjectFactory
 import factory
 
-from apps.projects.models import Project
+from apps.projects.models import Project, PartnerOrganization
 
 
 
 class OnePercentProjectFactory(ProjectFactory):
-
     FACTORY_FOR = Project
+
     deadline = timezone.now() + timedelta(days=30)
     amount_needed = 100
     amount_asked = 100
     allow_overfunding = True
+
+class PartnerFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = PartnerOrganization
+
+    name = factory.Sequence(lambda n: 'Partner_{0}'.format(n))
+    slug = factory.Sequence(lambda n: 'partner-{0}'.format(n))
+    description = 'Partner Organization factory model'
