47c963660a4fb345e8a061d073e80003732647b6
rollick <rollick@users.noreply.github.com>
2014-09-04 11:11:38 +0200

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -49,6 +49,9 @@ env.database = 'onepercentsite'
 # By default, confirm everywhere
 env.noinput = False
 
+# Use bash
+env.shell = "/bin/bash -c"
+
 # Utility functions:
 @contextmanager
 def virtualenv():
