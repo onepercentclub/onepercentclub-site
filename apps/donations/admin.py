f72a85c5fff0df208bd0e6be15f2a005757971b6
Loek van Gent <loek@1procentclub.nl>
2014-10-02 11:37:30 +0200

    Add MonthlyDonors and migrate from RecDDPayments + Orders

--- apps/donations/admin.py
+++ apps/donations/admin.py
@@ -1,5 +1,63 @@
-from apps.donations.models import MonthlyDonation, MonthlyBatch, MonthlyOrder
+from apps.donations.models import MonthlyDonation, MonthlyBatch, MonthlyOrder, MonthlyDonor, MonthlyDonorProject
 from django.contrib import admin
+from django.contrib.admin.filters import SimpleListFilter
+from django.utils.translation import ugettext as _
+
+
+class MonthlyDonorProjectInline(admin.TabularInline):
+    model = MonthlyDonorProject
+    readonly_fields = ('project', )
+    fields = readonly_fields
+    extra = 0
+
+
+class ActiveFilter(SimpleListFilter):
+    title = _('Active')
+
+    parameter_name = 'active__exact'
+    active_choices = (('1', _('Yes')),
+                      ('0', _('No')),)
+    default = '1'
+
+    def lookups(self, request, model_admin):
+        return (('all', _('All')),) + self.active_choices
+
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup if self.value() else lookup == self.default,
+                'query_string': cl.get_query_string({self.parameter_name: lookup}, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        if self.value() in ('0', '1'):
+            return queryset.filter(active=self.value())
+        elif self.value() is None:
+            return queryset.filter(active=self.default)
+
+
+class MonthlyDonorAdmin(admin.ModelAdmin):
+    model = MonthlyDonor
+    list_display = ('user', 'amount', 'active', 'iban', 'selected_projects')
+    raw_id_fields = ('user', )
+    inlines = (MonthlyDonorProjectInline, )
+    list_filter = (ActiveFilter, )
+    search_fields = ('user__first_name', 'user__email', 'iban')
+
+    def selected_projects(self, obj):
+        return obj.projects.count() or '-'
+
+admin.site.register(MonthlyDonor, MonthlyDonorAdmin)
+
+
+class MonthlyDonorProjectAdmin(admin.ModelAdmin):
+    model = MonthlyDonorProject
+    list_display = ('donor', 'project')
+    readonly_fields = ('donor', )
+    raw_id_fields = ('project', )
+
+admin.site.register(MonthlyDonorProject, MonthlyDonorProjectAdmin)
 
 
 class MonthlyBatchAdmin(admin.ModelAdmin):
--- apps/donations/models.py
+++ apps/donations/models.py
@@ -1,9 +1,35 @@
 from django.conf import settings
 from django.db import models
-from django.db.models.aggregates import Sum
 from django_extensions.db.fields import CreationDateTimeField, ModificationDateTimeField
 from django_iban.fields import IBANField, SWIFTBICField
 from django.utils.translation import ugettext as _
+
+
+class MonthlyDonor(models.Model):
+    """
+    Information about a user that wants to donate monthly.
+    """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+    created = CreationDateTimeField(_("Created"))
+    updated = ModificationDateTimeField(_("Updated"))
+
+    active = models.BooleanField(default=False)
+    amount = models.DecimalField(_("amount"), max_digits=6, decimal_places=2)
+
+    iban = IBANField(blank=True, default='')
+    bic = SWIFTBICField(blank=True, default='')
+    name = models.CharField(max_length=35)
+    city = models.CharField(max_length=35)
+
+
+class MonthlyDonorProject(models.Model):
+    """
+    Preferred projects by a monthly donor.
+    """
+
+    donor = models.ForeignKey(MonthlyDonor, related_name='projects')
+    project = models.ForeignKey(settings.PROJECTS_PROJECT_MODEL)
 
 
 class MonthlyBatch(models.Model):
@@ -14,6 +40,10 @@ class MonthlyBatch(models.Model):
 
     def __unicode__(self):
         return "{0} {1}".format(self.id, self.date)
+
+    class Meta:
+        verbose_name = _('Monthly batch')
+        verbose_name_plural = _('Monthly batches')
 
 
 class MonthlyOrder(models.Model):
