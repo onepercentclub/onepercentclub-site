a02c21f1359392e90c64e61f4afa48cefac5e39a
Marco <marco@onepercentclub.com>
2014-10-06 14:43:07 +0200

    CLUB-820, #resolved

--- apps/fundraisers/fundwallmails.py
+++ apps/fundraisers/fundwallmails.py
@@ -1,9 +1,9 @@
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 from apps.fundraisers.models import FundRaiser
 
 from bluebottle.wallposts.notifiers import WallPostObserver, ReactionObserver, ObserversContainer
 from bluebottle.mail import send_mail
-
 
 
 #TODO: fixed should work (fix text email)
@@ -15,9 +15,8 @@ class FundraiserWallObserver(WallPostObserver):
         WallPostObserver.__init__(self, instance)
 
     def notify(self):
-        print 'FundraiserWallNotificator'
         fundraiser = self.post
-        fundraiser_owner = fundraiser.author
+        fundraiser_owner = fundraiser.owner
 
         # Implement 1a: send email to Object owner, if Wallpost author is not the Object owner.
         if self.author != fundraiser_owner:
@@ -27,10 +26,11 @@ class FundraiserWallObserver(WallPostObserver):
                 to=fundraiser_owner,
 
                 project=fundraiser,
-                link='/go/projects/{0}'.format(fundraiser.slug),
+                link='/go/projects/{0}'.format(fundraiser.id),
                 author=self.author,
                 receiver=fundraiser_owner
             )
+
 
 #TODO: Just copied from others fix this
 class FundraiserReactionObserver(ReactionObserver):
@@ -41,26 +41,25 @@ class FundraiserReactionObserver(ReactionObserver):
         ReactionObserver.__init__(self, instance)
 
     def notify(self):
-        print 'FundraiserReactionWallNotificator'
-        project = self.post.content_object
-        project_owner = project.owner
+        fundraiser = self.post.content_object
+        fundraiser_owner = fundraiser.owner
 
         # Make sure users only get mailed once!
         mailed_users = set()
 
         # Implement 2c: send email to other Reaction authors that are not the Object owner or the post author.
         reactions = self.post.reactions.exclude(Q(author=self.post_author) |
-                                                Q(author=project_owner) |
+                                                Q(author=fundraiser_owner) |
                                                 Q(author=self.reaction_author))
         for r in reactions:
             if r.author not in mailed_users:
                 send_mail(
-                    template_name='project_wallpost_reaction_same_wallpost.mail',
+                    template_name='fundraiser_wallpost_reaction_same_wallpost.mail',
                     subject=_('%(author)s commented on a post you reacted on.') % {'author': self.reaction_author.get_short_name()},
                     to=r.author,
 
-                    project=project,
-                    link='/go/projects/{0}'.format(project.slug),
+                    project=fundraiser,
+                    link='/go/projects/{0}'.format(fundraiser.id),
                     author=self.reaction_author,
                     receiver=r.author
                 )
@@ -70,12 +69,12 @@ class FundraiserReactionObserver(ReactionObserver):
         if self.reaction_author != self.post_author:
             if self.reaction_author not in mailed_users and self.post_author:
                 send_mail(
-                    template_name='project_wallpost_reaction_new.mail',
+                    template_name='fundraiser_wallpost_reaction_new.mail',
                     subject=_('%(author)s commented on your post.') % {'author': self.reaction_author.get_short_name()},
                     to=self.post_author,
 
-                    project=project, #doesn't look like we need this
-                    link='/go/projects/{0}'.format(project.slug),
+                    project=fundraiser,
+                    link='/go/projects/{0}'.format(fundraiser.id),
                     site=self.site,
                     author=self.reaction_author,
                     receiver=self.post_author
@@ -83,17 +82,17 @@ class FundraiserReactionObserver(ReactionObserver):
                 mailed_users.add(self.post_author)
 
         # Implement 2a: send email to Object owner, if Reaction author is not the Object owner.
-        if self.reaction_author != project_owner:
-            if project_owner not in mailed_users:
+        if self.reaction_author != fundraiser_owner:
+            if fundraiser_owner not in mailed_users:
                 send_mail(
-                    template_name='project_wallpost_reaction_project.mail',
-                    subject=_('%(author)s commented on your project page.') % {'author': self.reaction_author.get_short_name()},
-                    to=project_owner,
+                    template_name='fundraiser_wallpost_reaction_fundraiser.mail',
+                    subject=_('%(author)s commented on your fundraiser page.') % {'author': self.reaction_author.get_short_name()},
+                    to=fundraiser_owner,
 
                     site=self.site,
-                    link='/go/projects/{0}'.format(project.slug),
+                    link='/go/projects/{0}'.format(fundraiser.id),
                     author=self.reaction_author,
-                    receiver=project_owner
+                    receiver=fundraiser_owner
                 )
 
 
