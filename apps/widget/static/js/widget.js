49d46b59014580b147d1ef0b75f43192a8937011
Loek van Gent <loek@1procentclub.nl>
2014-10-15 22:40:28 +0200

    Tweak widget

---
+++ apps/widget/static/js/widget.js
@@ -0,0 +1,49 @@
+(function() {
+
+    var jQuery;
+
+    if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.11.1') {
+        var script_tag = document.createElement('script');
+        script_tag.setAttribute("type", "text/javascript");
+        script_tag.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js");
+
+        if (script_tag.readyState) {
+            script_tag.onreadystatechange = function (){ //Specific for old versions of IE
+                if (this.readyState == 'complete' || this.readyState == 'loaded') {
+                    scriptLoadHandler();
+                }
+            };
+        } else { //Other browsers use this
+            script_tag.onload = scriptLoadHandler;
+        }
+
+        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+    } else {
+        jQuery = window.jQuery;
+        main();
+    }
+
+    function scriptLoadHandler() {
+        jQuery = window.jQuery.noConflict(true);
+        main();
+    }
+
+    function main(){
+        jQuery(document).ready(function($){
+            $('.widget-container').each(function(){
+                var el = $(this);
+                var project = el.data('project') ? el.data('project') : '';
+                var width = el.data('width') ? el.data('width') : 200;
+                var height = el.data('height') ? el.data('height') : 300;
+                var partner = el.data('partner') ? el.data('partner') : '';
+                var language = el.data('language') ? el.data('language') : 'en';
+                // var jsonp_url = "http://staging.onepercentclub.com/embed?callback=?&id=" + id + "&width=" + width + "&height=" + height
+                var jsonp_url = "http://localhost:8000/embed?callback=?&project=" + project + "&width=" + width + "&height=" + height +"&partner=" + partner + '&language=' + language;
+                $.getJSON(jsonp_url, function(data){
+                    el.html(data.html);
+                });
+            });
+        });
+    }
+
+})();
--- apps/widget/views.py
+++ apps/widget/views.py
@@ -1,3 +1,4 @@
+from bluebottle.bb_projects.models import ProjectPhase
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.views.generic import View
@@ -5,6 +6,7 @@ from django.utils import timezone
 from django.utils import translation
 from apps.projects.models import Project
 import json
+
 
 class WidgetView(View):
     template = "widget/widget.html"
@@ -18,7 +20,11 @@ class WidgetView(View):
         id = request.GET.get('id', None)
         height = request.GET.get('height', None)
         width  = request.GET.get('width', None)
+        widget = request.GET.get('widget', 'project')
+
         partner = request.GET.get('partner', None)
+        project_slug = request.GET.get('project', None)
+
         language = request.GET.get('language', None)
 
         if language:
@@ -28,13 +34,15 @@ class WidgetView(View):
         projects = None
         now = timezone.now()
 
-        if id and not partner:
+        allowed_phases = ProjectPhase.objects.filter(slug__in=['running', 'campaign'])
+
+        if project_slug:
             try:
-                projects = Project.filter(id=id, deadline__gt=now) #Enforce that we always get a QuerySet for use in the template
+                projects = Project.objects.filter(slug=project_slug)
             except Project.DoesNotExist:
                 pass
         elif partner:
-            projects = Project.objects.filter(partner_organization__slug=partner, deadline__gt=now).order_by('?')[:3]
+            projects = Project.objects.filter(partner_organization__slug=partner, status__in=allowed_phases).order_by('?')[:3]
         else:
             projects = Project.objects.filter(deadline__gt=now).order_by('?')[:3]            
 
@@ -43,4 +51,5 @@ class WidgetView(View):
         #Re-activate the previous language
         translation.activate(cur_language)
         response_data = "%s ( {'html': %s } )" % (callback, json.dumps(html))
-        return HttpResponse(response_data, content_type="text/javascript") 
+        return HttpResponse(response_data, content_type="text/javascript")
+
