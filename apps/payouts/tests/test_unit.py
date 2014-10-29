2f8503244f1c5b2427e139206f60e2bcfa1eac1f
Loek van Gent <loek@1procentclub.nl>
2014-10-29 11:42:05 +0100

    Fix status

--- apps/payouts/tests/test_unit.py
+++ apps/payouts/tests/test_unit.py
@@ -477,7 +477,7 @@ class OrganizationPayoutTestCase(OnePercentTestCase):
 
         # Change payout status to complete
         self.payout = Payout.objects.all()[0]
-        self.payout.status = PayoutLineStatuses.in_progress
+        self.payout.status = PayoutLineStatuses.progress
         self.payout.save()
         self.payout.status = PayoutLineStatuses.completed
         self.payout.save()
