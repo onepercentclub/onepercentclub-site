28ce3528f26515e51af73eca414665dd149ab55c
rollick <mark@onepercentclub.com>
2014-11-25 15:36:54 +0100

    reshuffle settings to have a common templates settings file for compressing / caching

--- onepercentclub/settings/server.py
+++ onepercentclub/settings/server.py
@@ -1,28 +1,9 @@
 from .base import *
+from .templates import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-COMPRESS_TEMPLATES = True
 
 INSTALLED_APPS += (
     'gunicorn',
 )
-
-COMPRESS_PRECOMPILERS = (
-    ('text/x-handlebars', 'embercompressorcompiler.filter.EmberHandlebarsCompiler'),
-)
-
-TEMPLATE_LOADERS = [
-    ('django.template.loaders.cached.Loader', (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-        'apptemplates.Loader', # extend AND override templates
-    )),
-]
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
--- onepercentclub/settings/test_runner.py
+++ onepercentclub/settings/test_runner.py
@@ -4,19 +4,10 @@
 # TODO: Use this with the Jenkins settings file
 #
 
-
-
-
 SECRET_KEY = 'hbqnTEq+m7Tk61bvRV/TLANr3i0WZ6hgBXDh3aYpSU8m+E1iCtlU3Q=='
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    },
-}
 
 from .base import *
-
+from .templates import *
 
 # Supress naive date warnings
 import warnings
@@ -26,25 +17,12 @@ warnings.filterwarnings(
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-COMPRESS_TEMPLATES = True
 
-COMPRESS_PRECOMPILERS = (
-    ('text/x-handlebars', 'embercompressorcompiler.filter.EmberHandlebarsCompiler'),
-)
-
-TEMPLATE_LOADERS = [
-    ('django.template.loaders.cached.Loader', (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-        'apptemplates.Loader', # extend AND override templates
-    )),
-]
-
-CACHES = {
+DATABASES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
 }
 
 # Selenium settings
@@ -57,8 +35,7 @@ INSTALLED_APPS += (
     'bluebottle.payments_mock'
 )
 
-from bluebottle.payments_mock.settings import MOCK_PAYMENT_METHODS, MOCK_FEES
-
 #Override the payment methods that are in payments.py in the settings directory
+from bluebottle.payments_mock.settings import MOCK_PAYMENT_METHODS, MOCK_FEES
 PAYMENT_METHODS = MOCK_PAYMENT_METHODS
 VAT_RATE = 0.21
