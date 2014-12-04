3d8d827471b23f5c86c0087f1d5a64199e3f81a5
Loek van Gent <loek@1procentclub.nl>
2014-12-04 10:59:27 +0100

    Take screenshot

--- apps/core/tests/test_functional.py
+++ apps/core/tests/test_functional.py
@@ -80,6 +80,8 @@ class PositiveDonationFlow(OnePercentSeleniumTestCase):
 
         self.scroll_to_and_click_by_css('a.btn-ok')
 
+        time.sleep(10)
+
         self.upload_screenshot()
 
         self.assertTrue(self.browser.is_text_present('Thanks for your support', wait_time=30))
