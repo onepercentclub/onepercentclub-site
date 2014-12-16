8fd428f3b418fb6e7e48f1ca5a20d35690ab7ddf
rollick <mark@onepercentclub.com>
2014-12-16 17:51:33 +0100

    remove js tests - they are being maintained in BB

--- test/js/factories/project_factory.js
+++
@@ -1,9 +0,0 @@
-Factory.define('project', {
-    // created: Date.now(),
-    owner: function() {
-        return attr('userPreview');
-    },
-    slug: 'empire-strikes-back',
-    title: 'Empire Strikes Back',
-    phase: 'Episode Five'
-});
