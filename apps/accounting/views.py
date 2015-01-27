783b53552efda3a2ac3d0d454510895b10a3d168
Joeri Bekker <joeri@maykinmedia.nl>
2015-01-27 22:28:49 +0100

    Added overviews with first priority.

--- apps/accounting/views.py
+++ apps/accounting/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum
+from django.db.models import Sum, Count
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import FormView
@@ -46,26 +46,37 @@ class AccountingOverviewView(FormView):
         form = kwargs.get('form')
 
         statistics = {}
-        pending = {}
 
         if form and form.is_valid():
             start = form.get_start()
             stop = form.get_stop()
 
             order_payments = OrderPayment.objects.filter(created__gte=start, created__lte=stop, status='settled')
-            bank_transactions = BankTransaction.objects.filter(book_date__gte=start, book_date__lte=stop)
-            remote_docdata_payments = RemoteDocdataPayment.objects.filter(local_payment__created__gte=start, local_payment__created__lte=stop, local_payment__status='settled')
-            remote_docdata_payouts = RemoteDocdataPayout.objects.filter(payout_date__gte=start, payout_date__lte=stop)
-            project_payouts = ProjectPayout.objects.filter(created__gte=start, created__lte=stop, status='settled') #
-            donations = Donation.objects.filter(created__gte=start, created__lte=stop, order__status='success')
+            order_payments_aggregated = order_payments.aggregate(Sum('amount'), Sum('transaction_fee'))
 
+            bank_transactions = BankTransaction.objects.filter(book_date__gte=start, book_date__lte=stop)
             bank_credit = bank_transactions.filter(credit_debit='C').aggregate(Sum('amount'))['amount__sum']
             bank_debit = bank_transactions.filter(credit_debit='D').aggregate(Sum('amount'))['amount__sum']
 
+            bank_cp = bank_transactions.filter(category_id=1)
+            bank_cp_credit = bank_cp.filter(credit_debit='C').aggregate(Sum('amount'))['amount__sum']
+            bank_cp_debit = bank_cp.filter(credit_debit='D').aggregate(Sum('amount'))['amount__sum']
+
+            remote_docdata_payments = RemoteDocdataPayment.objects.filter(remote_payout__payout_date__gte=start, remote_payout__payout_date__lte=stop)
+            remote_docdata_payments_aggregated = remote_docdata_payments.aggregate(Sum('amount_collected'), Sum('docdata_fee'), Sum('tpci'))
+
+            remote_docdata_payouts = RemoteDocdataPayout.objects.filter(payout_date__gte=start, payout_date__lte=stop)
+            remote_docdata_payouts_aggregated = remote_docdata_payouts.aggregate(Sum('payout_amount'))
+
+            project_payouts = ProjectPayout.objects.filter(created__gte=start, created__lte=stop, status='settled') #
+            project_payouts_aggregated = project_payouts.aggregate(Sum('amount_raised'), Sum('amount_payable'), Sum('organization_fee'))
+
+            donations = Donation.objects.filter(created__gte=start, created__lte=stop, order__status='success')
+
             statistics.update({
                 'orders': {
-                    'total_amount': order_payments.aggregate(Sum('amount'))['amount__sum'],
-                    'transaction_fee': order_payments.aggregate(Sum('transaction_fee'))['transaction_fee__sum'],
+                    'total_amount': order_payments_aggregated['amount__sum'],
+                    'transaction_fee': order_payments_aggregated['transaction_fee__sum'],
                     'count': order_payments.count(),
                 },
                 'donations': {
@@ -73,57 +84,61 @@ class AccountingOverviewView(FormView):
                     'count': donations.count(),
                 },
                 'bank': {
+                    'campaign_payout': {
+                        'credit': bank_cp_credit,  # in
+                        'debit': bank_cp_debit,    # out
+                        'balance': bank_cp_credit - bank_cp_debit,
+                        'count': bank_cp.count(),
+                    },
                     'credit': bank_credit,  # in
                     'debit': bank_debit,    # out
                     'balance': bank_credit - bank_debit,
                     'count': bank_transactions.count(),
                 },
-                'docdata_payments': {
-                    'total_amount': remote_docdata_payments.aggregate(Sum('amount_collected'))['amount_collected__sum'],
-                    'docdata_fee': remote_docdata_payments.aggregate(Sum('docdata_fee'))['docdata_fee__sum'],
-                    'count': remote_docdata_payments.count()
-                },
-                'docdata_payouts': {
-                    'total_amount': remote_docdata_payouts.aggregate(Sum('payout_amount'))['payout_amount__sum'],
-                    'count': remote_docdata_payouts.count()
+                'docdata': {
+                    'payment': {
+                        'total_amount': remote_docdata_payments_aggregated['amount_collected__sum'],
+                        'docdata_fee': remote_docdata_payments_aggregated['docdata_fee__sum'],
+                        'third_party': remote_docdata_payments_aggregated['tpci__sum'],
+                        'count': remote_docdata_payments.count()
+                    },
+                    'payout': {
+                        'total_amount': remote_docdata_payouts_aggregated['payout_amount__sum'],
+                        'count': remote_docdata_payouts.count()
+                    },
                 },
                 'project_payouts': {
-                    'raised': project_payouts.aggregate(Sum('amount_raised'))['amount_raised__sum'],
-                    'paid': project_payouts.aggregate(Sum('amount_payable'))['amount_payable__sum'],
-                    'organization_fee': project_payouts.aggregate(Sum('organization_fee'))['organization_fee__sum'],
+                    'per_payout_rule': project_payouts.order_by('payout_rule').values('payout_rule').annotate(
+                        raised=Sum('amount_raised'),
+                        payable=Sum('amount_payable'),
+                        organization_fee=Sum('organization_fee'),
+                        count=Count('payout_rule'),
+                    ),
+                    'raised': project_payouts_aggregated['amount_raised__sum'],
+                    'payable': project_payouts_aggregated['amount_payable__sum'],
+                    'organization_fee': project_payouts_aggregated['organization_fee__sum'],
                     'count': project_payouts.count()
                 },
             })
 
-            pending.update({
-                # Balance (saldo) of pending (to be paid) projects (all orders minus all payments to projects)
-                'project': {
-                    'total_order': statistics['orders']['total_amount'],
-                    'total_payment': 0,
-                    'balance': 0
-                },
-                # Balance (saldo) of pending (to be paid) orders (pending or paid orders not yet processed by Docdata)
-                'orders': {
-                    'total_order': 0,
-                    'total_docdata': 0,
-                    'balance': 0,
-                },
-                # Balance (saldo) of pending (to be paid) service fees to Docdata
-                'fees_docdata': {
-                },
-                # Balance (saldo) of pending (to be paid) service fees to 1%Club
-                'fees_organization': {
-                },
-                # Balance (saldo) of pending (to be paid) by Docdata to 1%Club (not yet processed/recieved by Bank)
-                'docdata_payouts': {
-                }
-            })
+        # Tpci (third party costs)
+        # Tdf (docdata fee)
+
+        statistics['docdata']['pending_orders'] = \
+            statistics['orders']['total_amount'] - \
+            statistics['docdata']['payout']['total_amount']
+
+        statistics['docdata']['payout']['other_costs'] = \
+            statistics['docdata']['payment']['total_amount'] - \
+            statistics['docdata']['payment']['docdata_fee'] - \
+            statistics['docdata']['payment']['third_party'] - \
+            statistics['docdata']['payout']['total_amount']
+
 
         context = super(AccountingOverviewView, self).get_context_data(**kwargs)
         context.update({
              'app_label': 'accounting',
              'title': _('Accountancy Overview'),
              'statistics': statistics,
-             'pending': pending,
         })
         return context
